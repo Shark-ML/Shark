9ce7417075502b4facb799ddfe3007eb6b061fe9
oswinkrause <Ulfgard@users.noreply.github.com>
2013-07-18 11:23:57 +0000

    added automatic code snippet inclusion to documentation

---
+++ doc/tutToRst/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ulfi/Shark/doc/tut2rst
+# Build directory: /home/ulfi/Shark/doc/tut2rst
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- examples/Supervised/quickstartTutorial.cpp
+++ examples/Supervised/quickstartTutorial.cpp
@@ -1,42 +1,56 @@
+//###begin<includes>
 #include <shark/Data/Csv.h>
 #include <shark/Algorithms/Trainers/LDA.h>
-#include <iostream>
-
 using namespace shark;
+//###end<includes>
+
+#include <iostream>
 using namespace std;
 
 int main(){
 	//create a Dataset from the file "quickstartData"
+	//###begin<load_data>
 	ClassificationDataset data;
 	import_csv(data, "data/quickstartData.csv", LAST_COLUMN, " ");
+	//###end<load_data>
 	
 	//create a test and training partition of the data
+	//###begin<split_data>
 	ClassificationDataset test = splitAtElement(data,static_cast<std::size_t>(0.8*data.numberOfElements()));
-
+	//###end<split_data>
+	
+	//###begin<objects>
 	//create a classifier for the problem
 	LinearClassifier classifier;
 	//create the lda trainer
 	LDA lda;
+	//###end<objects>
 	//train the classifier using the training portion of the Data
+	//###begin<train>
 	lda.train(classifier,data);
+	//###end<train>
 
 
 	//now use the test data to evaluate the model
-	unsigned int correct = 0;
 	//loop over all points of the test set
 	//be aware that in this example a single point consists of an input and a label
 	//this code here is just for illustration purposes
+	//###begin<eval>
+	unsigned int correct = 0;
 	BOOST_FOREACH(ClassificationDataset::element_reference point, test.elements()){
 		unsigned int result = classifier(point.input);
 		if (result == point.label){
 			correct++;
 		}
 	}
-
+	//###end<eval>
+	
 	//print results
+	//###begin<outputs>
 	cout << "RESULTS: " << endl;
 	cout << "========\n" << endl;
 	cout << "test data size: " << test.numberOfElements() << endl;
 	cout << "correct classification: "<< correct << endl;
 	cout << "error rate: " << 1.0 - double(correct)/test.numberOfElements() << endl;
+	//###end<outputs>
 }
