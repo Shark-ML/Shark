4ae53d9ff72f62777a1001e550994185ba71bd87
oswinkrause <oswinkrause@f06a09e3-3943-0410-8714-a4bb87ba143f>
2013-07-19 12:52:21 +0000

    ported some more tutorials

--- trunk/Shark/examples/Unsupervised/BinaryRBM.cpp
+++ trunk/Shark/examples/Unsupervised/BinaryRBM.cpp
@@ -1,11 +1,15 @@
+//###begin<includes>
+//used for training the RBM
 #include <shark/Unsupervised/RBM/BinaryRBM.h>
-#include <shark/Unsupervised/RBM/Problems/BarsAndStripes.h>
 #include <shark/Algorithms/GradientDescent/SteepestDescent.h>
-#include <shark/Rng/GlobalRng.h>
+
+//the problem
+#include <shark/Unsupervised/RBM/Problems/BarsAndStripes.h>
 
 //for evaluation
 #include <shark/Unsupervised/RBM/analytics.h>
 #include <iostream>
+//###end<includes>
 
 using namespace shark;
 using namespace std;
@@ -21,34 +25,41 @@ void initializeWeights(BinaryRBM& rbm){
 
 int main(){
 
-	//we first create the problem. in this tutorial,
-	//we use BarsAndStripes
+	//we first create the problem. in this tutorial, we use BarsAndStripes
+	//###begin<problem>
 	BarsAndStripes problem;
 	UnlabeledData<RealVector> data = problem.data();
-
+	//###end<problem>
+	
 	//some constants needed for training
+	//###begin<RBM>
 	size_t numberOfHidden = 32;//hidden units of the rbm
 	size_t numberOfVisible = problem.inputDimension();//visible units of the inputs
-	unsigned int numIterations = 1000;//iterations for training
-	unsigned int numTrials = 10;//number of trials for training
 
 	//create rbm with simple binary units
 	BinaryRBM rbm(Rng::globalRng);
 	rbm.setStructure(numberOfVisible,numberOfHidden);
-
+	//###end<RBM>
+	
 	//create derivative to optimize the rbm
 	//we want a simple vanilla CD-1
+	//###begin<cd>
 	BinaryCD cd(&rbm);
 	cd.setK(1);
 	cd.setData(data);
-	
+	//###end<cd>
 
 	//generate optimizer
+	//###begin<optimizer>
 	SteepestDescent optimizer;
 	optimizer.setMomentum(0);
 	optimizer.setLearningRate(0.1);
-
+	//###end<optimizer>
+	
 	//now we train the rbm and evaluate the mean negative log-likelihood at the end
+	//###begin<train>
+	unsigned int numIterations = 1000;//iterations for training
+	unsigned int numTrials = 10;//number of trials for training
 	double meanResult = 0;
 	for(unsigned int trial = 0; trial != numTrials; ++trial) {
 		initializeWeights(rbm);
@@ -63,11 +74,14 @@ int main(){
 		meanResult +=likelihood;
 	}
 	meanResult /= numTrials;
+	//###end<train>
 
 	//print the mean performance
+	//###begin<output>
 	cout << "RESULTS: " << std::endl;
 	cout << "======== " << std::endl;
 	cout << "mean negative log likelihood: " << meanResult << std::endl;
+	//###end<output>
 
 }
 
