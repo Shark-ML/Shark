c6e7c94706d722da2cc160263032c0c9904fe31d
oswinkrause <oswinkrause@f06a09e3-3943-0410-8714-a4bb87ba143f>
2013-08-25 14:23:51 +0000

    added kernel kMeans

--- trunk/Shark/Test/Algorithms/KMeans.cpp
+++ trunk/Shark/Test/Algorithms/KMeans.cpp
@@ -39,6 +39,9 @@
 
 #include <shark/Algorithms/KMeans.h>
 #include <shark/Models/Clustering/HardClusteringModel.h>
+#include <shark/Models/Kernels/LinearKernel.h>
+#include <shark/Models/ConcatenatedModel.h>
+#include <shark/Models/Converter.h>
 
 using namespace shark;
 
@@ -135,6 +138,99 @@ BOOST_AUTO_TEST_CASE(KMeans_multiple_gauss)
 		//check that the means are the same
 		for (unsigned int i=0; i<numMeans; i++){
 			double distance = distanceSqr(clusterMeans[i],centroids.centroids().element(i));
+			BOOST_CHECK_SMALL(distance, 1.e-10);
+		}
+	}
+}
+
+
+BOOST_AUTO_TEST_CASE(Kernel_KMeans_multiple_gauss)
+{
+	const unsigned int numTrials = 100;
+	const unsigned int numPoints = 300;
+	const unsigned int numMeans = 3;
+	const unsigned int numDimensions = 5;
+	for(unsigned int trial = 0; trial != numTrials; ++trial){
+		//prepare means
+		std::vector<RealVector> means(numMeans,RealVector(numDimensions));
+		for (unsigned int i=0; i<numMeans; i++){
+			for (unsigned int j=0; j <numDimensions; j++){
+				means[i](j) = Rng::uni(0,5);
+			}
+		}
+		// prepare data set
+		std::vector<RealVector> data(numPoints);
+		for (std::size_t i=0; i<numPoints; i++)
+		{
+			data[i]=means[i%numMeans];
+			for (unsigned int j=0; j <numDimensions; j++){
+				data[i](j) += Rng::uni(0,1);
+			}
+		}
+		Data<RealVector> dataset = createDataFromRange(data);
+
+		// invoke k-means with random centroids and no upper limit for iterations
+		Centroids centroids;
+		LinearKernel<> kernel;
+		KernelExpansion<RealVector> clusteringModel = kMeans(dataset, numMeans, kernel);
+		//std::cout<<clusteringModel.alpha()<<std::endl;
+		// check result
+		BOOST_REQUIRE_EQUAL( clusteringModel.outputSize(), numMeans);
+		
+		//check that every point is assigned to exactly one cluster
+		UIntVector numAssignments = sumColumns(clusteringModel.alpha() != 0.0);
+		BOOST_REQUIRE_EQUAL(numAssignments.size(),numPoints);
+		for(std::size_t i = 0; i != numPoints; ++i){
+			BOOST_CHECK_EQUAL(numAssignments(i),1);
+		}
+		//check that the weights of every cluster sums to ~1
+		RealVector columnSums = sumRows(clusteringModel.alpha());
+		BOOST_REQUIRE_EQUAL(columnSums.size(),numMeans);
+		for(std::size_t i = 0; i != numMeans; ++i){
+			BOOST_CHECK_CLOSE(columnSums(i),2.0, 1.e-5);
+		}
+		
+		//now calculate the exact centers and check that the distances are 
+		//the same as the result of the kernel expansion
+		RealMatrix centers(numMeans,numDimensions,0.0);
+		for(std::size_t i = 0; i != numPoints; ++i){
+			centers+=0.5*outer_prod(row(clusteringModel.alpha(),i),data[i]);
+		}
+		
+		for(std::size_t b = 0; b != dataset.numberOfBatches(); ++b){
+			RealMatrix const& batch = dataset.batch(b);
+			RealMatrix modelResult = clusteringModel(batch);
+			RealMatrix kernelResult = -kernel.featureDistanceSqr(batch,centers);
+			BOOST_REQUIRE_EQUAL(modelResult.size1(),kernelResult.size1());
+			BOOST_REQUIRE_EQUAL(modelResult.size2(),kernelResult.size2());
+			//the distances are off by the size of the elements so we have to correct this 
+			//(it does not matter for clustering)
+			for(std::size_t i = 0; i != batch.size1();++i){
+				double distSqri = norm_sqr(row(batch,i));
+				row(modelResult,i) -= repeat(distSqri,numMeans);
+				BOOST_CHECK_SMALL(distanceSqr(row(modelResult,i),row(kernelResult,i)),1.e-5);
+			}
+		}
+		
+		//now check proper convergence of the algorithms, that means that cluster assignment
+		//is stable.
+		ArgMaxConverter converter;
+		ConcatenatedModel<RealVector,unsigned int> model = clusteringModel >> converter;
+		//assign centers
+		Data<unsigned int> clusters = model(dataset);
+		
+		//create new cluster means (they should be the same as the previous ones)
+		std::vector<RealVector> newClusterMeans(numMeans,RealVector(numDimensions,0));
+		std::vector<std::size_t> members = classSizes(clusters);
+		for (std::size_t i=0; i<numPoints; i++)
+		{
+			unsigned int id = clusters.element(i);
+			newClusterMeans[id]+=data[i]/members[id];
+		}
+		
+		//check that the means are the same
+		for (unsigned int i=0; i<numMeans; i++){
+			double distance = distanceSqr(newClusterMeans[i],row(centers,i));
 			BOOST_CHECK_SMALL(distance, 1.e-10);
 		}
 	}
--- trunk/Shark/include/shark/Algorithms/KMeans.h
+++ trunk/Shark/include/shark/Algorithms/KMeans.h
@@ -40,6 +40,8 @@
 
 #include <shark/Data/Dataset.h>
 #include <shark/Models/Clustering/Centroids.h>
+#include <shark/Models/Kernels/KernelExpansion.h>
+#include <shark/Models/Kernels/KernelHelpers.h>
 
 
 namespace shark{
@@ -73,9 +75,93 @@ namespace shark{
 /// \param maxIterations  maximum number of k-means iterations; 0: unlimited
 /// \return               number of k-means iterations
 ///
-
 std::size_t kMeans(Data<RealVector> const& data, std::size_t k, Centroids& centroids, std::size_t maxIterations = 0);
 
+template<class InputType>
+KernelExpansion<InputType> kMeans(Data<InputType> const& dataset, std::size_t k, AbstractKernelFunction<InputType>& kernel, std::size_t maxIterations = 0){
+	if(!maxIterations)
+		maxIterations = std::numeric_limits<std::size_t>::max();
+	
+	std::size_t ell = dataset.numberOfElements();
+	RealMatrix kernelMatrix = calculateRegularizedKernelMatrix(kernel,dataset,0);
+	UIntVector clusterMembership(ell);
+	UIntVector clusterSizes(k,0);
+	RealVector ckck(k,0);
+	
+	//init cluster assignments
+	for(std::size_t i = 0; i != ell; ++i){
+		clusterMembership(i) = i % k;
+	}
+	DiscreteUniform<Rng::rng_type> uni(Rng::globalRng,0,k-1);
+	std::random_shuffle(clusterMembership.begin(),clusterMembership.end(),uni);
+	for(std::size_t i = 0; i != ell; ++i){
+		++clusterSizes(clusterMembership(i));
+	}
+	
+	// k-means loop
+	std::size_t iter = 0;
+	bool equal = false;
+	for(; iter != maxIterations && !equal; ++iter) {
+		//the clustering model results in centers c_k= 1/n_k sum_i k(x_i,.) for all x_i points of cluster k
+		//we need to compute the squared distances between all centers and points that is
+		//d^2(c_k,x_i) = <c_k,c_k> -2 < c_k,x_i> + <x_i,x_i> for the i-th point.
+		//thus we precompute <c_k,c_k>= sum_ij k(x_i,x_j)/(n_k)^2 for all x_i,x_j points of cluster k
+		zero(ckck);
+		for(std::size_t i = 0; i != ell; ++i){
+			std::size_t c1 = clusterMembership(i);
+			for(std::size_t j = 0; j != ell; ++j){
+				std::size_t c2 = clusterMembership(j);
+				if(c1 != c2) continue;
+				ckck(c1) += kernelMatrix(i,j);
+			}
+		}
+		noalias(ckck) = safeDiv(ckck,sqr(clusterSizes),0);
+		
+		UIntVector newClusterMembership(kernelMatrix.size1());
+		RealVector currentDistances(k);
+		for(std::size_t i = 0; i != ell; ++i){
+			//compute squared distances between the i-th point and the centers
+			 //we skip <x_i,x_i> as it is always the same for all elements and we don't need it for comparison
+			noalias(currentDistances) = ckck;
+			for(std::size_t j = 0; j != ell; ++j){
+				std::size_t c = clusterMembership(j);
+				currentDistances(c) -= 2* kernelMatrix(i,j)/clusterSizes(c);
+			}
+			//choose the index with the smallest distance as new cluster
+			newClusterMembership(i) = arg_min(currentDistances);
+		}
+		equal = boost::equal(
+			newClusterMembership,
+			clusterMembership
+		);
+		noalias(clusterMembership) = newClusterMembership;
+		//compute new sizes of clusters
+		zero(clusterSizes);
+		for(std::size_t i = 0; i != ell; ++i){
+			++clusterSizes(clusterMembership(i));
+		}
+		
+		//if a cluster has size , assign a random point to it
+		for(std::size_t i = 0; i != k; ++i){
+			if(clusterSizes(i) == 0){
+				std::size_t elem = uni(ell-1);
+				--clusterSizes(clusterMembership(elem));
+				clusterMembership(elem)=i;
+				clusterSizes(i) = 1;
+			}
+		}
+	}
+	
+	//copy result in the expansion
+	KernelExpansion<InputType> expansion(&kernel,dataset,true,k);
+	expansion.offset() = -ckck;
+	zero(expansion.alpha());
+	for(std::size_t i = 0; i != ell; ++i){
+		std::size_t c = clusterMembership(i);
+		expansion.alpha()(i,c) = 2.0 / clusterSizes(c);
+	}
 
+	return expansion;
+}
 }
 #endif
