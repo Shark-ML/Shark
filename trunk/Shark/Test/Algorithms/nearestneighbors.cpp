53648422891878aa46e4ace5c40c9b2d3ea8599c
oswinkrause <oswinkrause@f06a09e3-3943-0410-8714-a4bb87ba143f>
2013-12-19 10:01:36 +0000

    fixed dysfunctional nearest neighbors test.

--- trunk/Shark/Test/Algorithms/nearestneighbors.cpp
+++ trunk/Shark/Test/Algorithms/nearestneighbors.cpp
@@ -18,7 +18,7 @@
 *  GNU General Public License for more details.
 *
 *  You should have received a copy of the GNU General Public License
-*  along with this library; if not, see <http://www.gnu.org/licenses/>.
+*  along with this library; if not, see <http:www.gnu.org/licenses/>.
 *  
 */
 //===========================================================================
@@ -81,24 +81,23 @@ void testTree(
 		std::pair<double,std::size_t> ret = query.next();
 		dist[i] = ret.first;
 		neighbor[i] = &data[ret.second];
-		std::cout<<dist[i]<<" "<<ret.second<<std::endl;
 	}
 	double time = Timer::now() - start;
 
 	// check consistency with brute force algorithm
-	/*for (std::size_t i=0; i<neighbors; i++)
+	for (std::size_t i=0; i<NEIGHBORS; i++)
 	{
 		if (i >= 5) { 
-			boost_check_equal(&data[index[i]], neighbor[i]);
+			BOOST_CHECK_EQUAL(&data[index[i]], neighbor[i]);
 		}
-		boost_check_small(distance(*neighbor[i], test) - dist[i], 1e-12);
+		BOOST_CHECK_SMALL(distance(*neighbor[i], test) - dist[i], 1e-12);
 	}
 
-	 check consistency of ranking
-	for (std::size_t i=1; i<neighbors; i++)
+	// check consistency of ranking
+	for (std::size_t i=1; i<NEIGHBORS; i++)
 	{
-		boost_check_le(dist[i-1], dist[i]);
-	}*/
+		BOOST_CHECK_LE(dist[i-1], dist[i]);
+	}
 
 	// more than 40x faster on my machine
 	BOOST_TEST_MESSAGE(name<<" time: " << time << " seconds, versus " << time_reference << " seconds for brute force search");
@@ -106,7 +105,7 @@ void testTree(
 }
 
 
-//chck that the tree locates every of it's training points correctly
+//check that the tree locates every of it's training points correctly
 template<class Tree>
 void testTreeStructure(
 	Tree const& tree, std::vector<RealVector> const& data
@@ -128,84 +127,65 @@ void testTreeStructure(
 	}
 }
 
+
 // Generate 100.000 i.i.d. standard normally
 // distributed samples in 3D, then query the
 // 1.000 nearest neighbors of the origin.
 BOOST_AUTO_TEST_CASE(IterativeNearestNeighborQueries)
 {
+	double start;
 	Rng::seed(42);
-	// generate data set and test point
-	std::vector<RealVector> data(100);
-	for (std::size_t i=0; i<100; i++)
+	// generate data set and test set
+	std::vector<RealVector> data(TRAINING);
+	std::vector<RealVector> test(10);
+	for (std::size_t i=0; i<5; i++)
 	{
 		// multiple instances of the same point
-		data[i].resize(1);
+		data[i].resize(3);
+		data[i][0] = 0.0;
+		data[i][1] = 0.0;
+		data[i][2] = 0.0;
+	}
+	for (std::size_t i=0; i<10; i++)
+	{
+		// multiple instances of the same point
+		test[i].resize(3);
+		test[i][0] = Rng::gauss();
+		test[i][1] = Rng::gauss();
+		test[i][2] = Rng::gauss();
+	}
+	test[0].clear();//(0,0,0)
+	for (std::size_t i=0; i<TRAINING; i++)
+	{
+		// random data
+		data[i].resize(3);
 		data[i][0] = Rng::gauss();
-		//data[i][1] = Rng::gauss();
+		data[i][1] = Rng::gauss();
+		data[i][2] = Rng::gauss();
 	}
 
 	UnlabeledData<RealVector> dataset = createDataFromRange(data);
 
-	// test the KDTree
+	//test trees
 	KDTree<RealVector> kdtree(dataset);
 	testTreeStructure(kdtree,data);
-//	LCTree<RealVector> lctree(data);
-//	LinearKernel<RealVector> kernel;
-//	KHCTree<std::vector<RealVector> > khctree(data, &kernel);
-//	testTree(kdtree,"KDTree",data,test,index,time_reference);
-//	testTree(lctree,"LCTree",data,test,index,time_reference);
-//	testTree(khctree,"KHCTree",data,test,index,time_reference);
+	LCTree<RealVector> lctree(dataset);
+	LinearKernel<RealVector> kernel;
+	KHCTree<std::vector<RealVector> > khctree(data, &kernel);
+	
+	for(std::size_t k = 0; k != 10; ++k){
+		// brute force sorting (for comparison)
+		std::vector<std::size_t> index(TRAINING);
+		start = Timer::now();
+		for (std::size_t i=0; i<TRAINING; i++) 
+			index[i] = i;
+		Comparator comparator(data, test[k]);
+		std::sort(index.begin(), index.end(), comparator);
+		double time_reference = Timer::now() - start;
+		
+		std::cout<<"Test: "<<k<<std::endl;
+		testTree(kdtree,"KDTree",data,test[k],index,time_reference);
+		testTree(lctree,"LCTree",data,test[k],index,time_reference);
+		testTree(khctree,"KHCTree",data,test[k],index,time_reference);
+	}
 }
-
-
-// Generate 100.000 i.i.d. standard normally
-// distributed samples in 3D, then query the
-// 1.000 nearest neighbors of the origin.
-//BOOST_AUTO_TEST_CASE(IterativeNearestNeighborQueries)
-//{
-//	double start;
-//	Rng::seed(42);
-//	// generate data set and test point
-//	std::vector<RealVector> data(TRAINING);
-//	for (std::size_t i=0; i<5; i++)
-//	{
-//		// multiple instances of the same point
-//		data[i].resize(3);
-//		data[i][0] = 0.0;
-//		data[i][1] = 0.0;
-//		data[i][2] = 0.0;
-//	}
-//	for (std::size_t i=0; i<TRAINING; i++)
-//	{
-//		// random data
-//		data[i].resize(3);
-//		data[i][0] = Rng::gauss();
-//		data[i][1] = Rng::gauss();
-//		data[i][2] = Rng::gauss();
-//	}
-//	RealVector test(3);
-//	test[0] = 0.0;
-//	test[1] = 0.0;
-//	test[2] = 0.0;
-//
-//	// brute force sorting (for comparison)
-//	std::vector<std::size_t> index(TRAINING);
-//	start = Timer::now();
-//	for (std::size_t i=0; i<TRAINING; i++) 
-//		index[i] = i;
-//	Comparator comparator(data, test);
-//	std::sort(index.begin(), index.end(), comparator);
-//	double time_reference = Timer::now() - start;
-//
-//	UnlabeledData<RealVector> dataset(data);
-//
-//	// test the KDTree
-//	KDTree<RealVector> kdtree(dataset);
-//	testTreeStructure(kdtree,data);
-////	LCTree<RealVector> lctree(data);
-////	LinearKernel<RealVector> kernel;
-////	KHCTree<std::vector<RealVector> > khctree(data, &kernel);
-////	testTree(kdtree,"KDTree",data,test,index,time_reference);
-////	testTree(lctree,"LCTree",data,test,index,time_reference);
-////	testTree(khctree,"KHCTree",data,test,index,time_reference);
-//}
