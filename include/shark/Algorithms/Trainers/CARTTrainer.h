b4500cfada46262acf2b9260a77203717783b80d
oswinkrause <Ulfgard@users.noreply.github.com>
2014-01-31 12:43:59 +0000

    partially added code from Kang

--- include/shark/Algorithms/Trainers/CARTTrainer.h
+++ include/shark/Algorithms/Trainers/CARTTrainer.h
@@ -76,8 +76,8 @@ public:
 
 	/// Constructor
 	CARTTrainer(){
-		//Set the node size to 1 as default
 		m_nodeSize = 1;
+		m_numberOfFolds = 10;
 	}
 
 	/// \brief From INameable: return the class name.
@@ -88,74 +88,81 @@ public:
 	void train(ModelType& model, ClassificationDataset const& dataset);
 	
 	///Train regression
-    void train(ModelType& model, RegressionDataset const& dataset);
+	void train(ModelType& model, RegressionDataset const& dataset);
 
+	///Sets the number of folds used for creation of the trees.
+	void setNumberOfFolds(unsigned int folds){
+		m_numberOfFolds = folds;
+	}
 protected:
 
-    ///Types frequently used
-    struct TableEntry{
-    	double value;
-    	std::size_t id;
-    	
-    	bool operator<( TableEntry const& v2)const {
+	///Types frequently used
+	struct TableEntry{
+		double value;
+		std::size_t id;
+
+		bool operator<( TableEntry const& v2)const {
 			return value < v2.value;
 		}
-    };
-    typedef std::vector < TableEntry > AttributeTable;
-    typedef std::vector < AttributeTable > AttributeTables;
-    
-    typedef ModelType::SplitMatrixType SplitMatrixType;
-    
+	};
+	typedef std::vector < TableEntry > AttributeTable;
+	typedef std::vector < AttributeTable > AttributeTables;
 
-    ///Number of attributes in the dataset
-    std::size_t m_inputDimension;
+	typedef ModelType::SplitMatrixType SplitMatrixType;
 
-    ///Size of labels
-    std::size_t m_labelDimension;
 
-    ///Controls the number of samples in the terminal nodes
-    std::size_t m_nodeSize;
+	///Number of attributes in the dataset
+	std::size_t m_inputDimension;
 
-    ///Holds the maximum label. Used in allocating the histograms
-    unsigned int m_maxLabel;
+	///Size of labels
+	std::size_t m_labelDimension;
 
-    //Classification functions
-    ///Builds a single decision tree from a classification dataset
-    ///The method requires the attribute tables,
-    SplitMatrixType buildTree(AttributeTables const& tables, ClassificationDataset const& dataset, boost::unordered_map<std::size_t, std::size_t>& cAbove, std::size_t nodeId );
+	///Controls the number of samples in the terminal nodes
+	std::size_t m_nodeSize;
 
-    ///Calculates the Gini impurity of a node. The impurity is defined as
-    ///1-sum_j p(j|t)^2
-    ///i.e the 1 minus the sum of the squared probability of observing class j in node t
-    double gini(boost::unordered_map<std::size_t, std::size_t>& countMatrix, std::size_t n);
-    ///Creates a histogram from the count matrix.
-    RealVector hist(boost::unordered_map<std::size_t, std::size_t> countMatrix);
+	///Holds the maximum label. Used in allocating the histograms
+	unsigned int m_maxLabel;
 
-    ///Regression functions
-    SplitMatrixType buildTree(AttributeTables const& tables, RegressionDataset const& dataset, std::vector<RealVector> const& labels, std::size_t nodeId, std::size_t trainSize);
-    ///Calculates the total sum of squares
-    double totalSumOfSquares(std::vector<RealVector> const& labels, std::size_t start, std::size_t length, const RealVector& sumLabel);
-    ///Calculates the mean of a vector of labels
-    RealVector mean(std::vector<RealVector> const& labels);
+	///Number of folds used to create the tree.
+	unsigned int m_numberOfFolds;
 
-    ///Pruning
-    ///Prunes decision tree, represented by a split matrix
-    void pruneMatrix(SplitMatrixType& splitMatrix);
-    ///Prunes a single node, including the child nodes of the decision tree
-    void pruneNode(SplitMatrixType& splitMatrix, std::size_t nodeId);
-    ///Updates the node variables used in the cost complexity pruning stage
-    void measureStrenght(SplitMatrixType& splitMatrix, std::size_t nodeId, std::size_t parentNodeId);
-    
-    ///Returns the index of the node with node id in splitMatrix.
-    std::size_t findNode(SplitMatrixType& splitMatrix, std::size_t nodeId);
-    
-    ///Attribute table functions
-    ///Create the attribute tables used by the SPRINT algorithm
-    AttributeTables createAttributeTables(Data<RealVector> const& dataset);
-    ///Splits the attribute tables by a attribute index and value. Returns a left and a right attribute table in the variables LAttributeTables and RAttributeTables
-    void splitAttributeTables(AttributeTables const& tables, std::size_t index, std::size_t valIndex, AttributeTables& LAttributeTables, AttributeTables& RAttributeTables);
-    ///Crates count matrices from a classification dataset
-    boost::unordered_map<std::size_t, std::size_t> createCountMatrix(ClassificationDataset const& dataset);
+	//Classification functions
+	///Builds a single decision tree from a classification dataset
+	///The method requires the attribute tables,
+	SplitMatrixType buildTree(AttributeTables const& tables, ClassificationDataset const& dataset, boost::unordered_map<std::size_t, std::size_t>& cAbove, std::size_t nodeId );
+
+	///Calculates the Gini impurity of a node. The impurity is defined as
+	///1-sum_j p(j|t)^2
+	///i.e the 1 minus the sum of the squared probability of observing class j in node t
+	double gini(boost::unordered_map<std::size_t, std::size_t>& countMatrix, std::size_t n);
+	///Creates a histogram from the count matrix.
+	RealVector hist(boost::unordered_map<std::size_t, std::size_t> countMatrix);
+
+	///Regression functions
+	SplitMatrixType buildTree(AttributeTables const& tables, RegressionDataset const& dataset, std::vector<RealVector> const& labels, std::size_t nodeId, std::size_t trainSize);
+	///Calculates the total sum of squares
+	double totalSumOfSquares(std::vector<RealVector> const& labels, std::size_t start, std::size_t length, const RealVector& sumLabel);
+	///Calculates the mean of a vector of labels
+	RealVector mean(std::vector<RealVector> const& labels);
+
+	///Pruning
+	///Prunes decision tree, represented by a split matrix
+	void pruneMatrix(SplitMatrixType& splitMatrix);
+	///Prunes a single node, including the child nodes of the decision tree
+	void pruneNode(SplitMatrixType& splitMatrix, std::size_t nodeId);
+	///Updates the node variables used in the cost complexity pruning stage
+	void measureStrenght(SplitMatrixType& splitMatrix, std::size_t nodeId, std::size_t parentNodeId);
+
+	///Returns the index of the node with node id in splitMatrix.
+	std::size_t findNode(SplitMatrixType& splitMatrix, std::size_t nodeId);
+
+	///Attribute table functions
+	///Create the attribute tables used by the SPRINT algorithm
+	AttributeTables createAttributeTables(Data<RealVector> const& dataset);
+	///Splits the attribute tables by a attribute index and value. Returns a left and a right attribute table in the variables LAttributeTables and RAttributeTables
+	void splitAttributeTables(AttributeTables const& tables, std::size_t index, std::size_t valIndex, AttributeTables& LAttributeTables, AttributeTables& RAttributeTables);
+	///Crates count matrices from a classification dataset
+	boost::unordered_map<std::size_t, std::size_t> createCountMatrix(ClassificationDataset const& dataset);
 
 
 };
