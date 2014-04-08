0da440e9ddee8c97e039f9536c25688e750e2d0a
glasmtbl <glasmtbl@f06a09e3-3943-0410-8714-a4bb87ba143f>
2014-04-08 19:50:36 +0000

    documentation slightly altered

--- trunk/Shark/include/shark/ObjectiveFunctions/KernelBasisDistance.h
+++ trunk/Shark/include/shark/ObjectiveFunctions/KernelBasisDistance.h
@@ -37,21 +37,20 @@
 
 namespace shark{
 
-	
+
 /// \brief Computes the distance between the optimal point in a basis to the point represented by a KernelExpansion.
 ///
-/// Assume we are given a kernel expansion \f$ w= \sum_i \alpha_i k(x_i,\cdot) \f$. The KernelBasisDistance takes
+/// Assume we are given a kernel expansion \f$ w = \sum_i \alpha_i k(x_i, \cdot) \f$. The KernelBasisDistance takes
 /// a new set of basis vectors \f$ z_i \f$ and finds the linear combination in that space which is closest
-/// to \f$ w \f$ . More formally the function measures the squared distance in kernel space:
-/// \f[ f(z) = \min_{\beta} \frac 1 2 || \sum_j \beta_j k(z_j,\cdot)-w|| . \f]
-/// In vector notation with \f$ (K_x)_{i,j} = k(x_i,x_j) \f$ , \f$ (K_z)_{i,j} = k(z_i,z_j) \f$ and \f$ (K_{zx})_{i,j} = k(z_i,x_j) \f$ it computes:
+/// to \f$ w \f$ . More formally the function measures the squared distance in the kernel-induced feature space:
+/// \f[ f(z) = \min_{\beta} \frac 1 2 \| \sum_j \beta_j k(z_j, \cdot) - w \|^2 . \f]
+/// In vector notation with \f$ (K_x)_{i,j} = k(x_i,x_j) \f$, \f$ (K_z)_{i,j} = k(z_i,z_j) \f$ and \f$ (K_{zx})_{i,j} = k(z_i,x_j) \f$ it computes:
 ///\f[ f(z) = \min_{\beta} \frac 1 2  \beta^T K_z  \beta - \beta^T K_{zx} \alpha + \frac 1 2 \alpha^TK_x \alpha . \f]
-/// as the last term is constant in \f$ z_i \f$ , it is omitted in the actual computation that is, the value is offsetted by some constant and the minimum is not 0.
-/// The input of the function is a vector which continuously stores the set of points in the basis. That is, it stores
-/// \f$ v=[z_1, z_2,\dots,z_k] \f$.
+/// The last term is independent of \f$ z_i \f$. Thus it is omitted in the actual computation. That is, the value is offset by a constant and the minimum is not 0.
+/// The input of the function consists of a vector which is the concatenation \f$ v=[z_1, z_2,\dots,z_k] \f$ of all basis vectors.
 ///
-/// The target point \f$ w \f$ is set by the KernelExpansion in the constructor. When the kernel used by the expansion is differentiable
-/// with respect to the input point, this function is differentiable as well.
+/// The target point \f$ w \f$ is set as a KernelExpansion in the constructor. If the kernel is differentiable
+/// with respect to the input point then this objective function is differentiable as well.
 ///
 /// The kernel expansion can represent more than one single point, in this point the error is the sum of approximation errors.
 class KernelBasisDistance : public SingleObjectiveFunction
@@ -75,20 +74,21 @@ public:
 	///
 	/// Returns a random subset of the basis of the kernel expansion
 	void proposeStartingPoint(SearchPointType& startingPoint) const;
-	
+
 	/// \brief Returns the number of variables of the function.
 	std::size_t numberOfVariables()const;
 
-	/// \brief Evaluate the (sum of) distance(s) between the closes point in the basis to the point(s) represented by the kernel expansion.
+	/// \brief Evaluate the (sum of) squared distance(s) between the closes point in the basis to the point(s) represented by the kernel expansion.
 	///
 	/// See the class description for more details on this computation.
 	double eval(RealVector const& input) const;
 
 	/// \brief computes the derivative of the function with respect to the supplied basis.
 	///
-	/// Assume \f$ \beta \f$ to be the optimal value. then the derivative with respect to the basis vectors is:
+	/// Assume \f$ \beta \f$ to be the optimal value. Then the derivative with respect to the basis vectors is:
 	/// \f[	\frac{ \partial f}{\partial z_l} = \beta_l \sum_i \beta_i \frac{ \partial f}{\partial z_l} k(z_l,z_i) - \beta_l \sum_i \alpha_i \frac{ \partial f}{\partial z_l} k(z_l, x_i) \f]
 	ResultType evalDerivative( const SearchPointType & input, FirstOrderDerivative & derivative ) const;
+
 private:
 	KernelExpansion<RealVector>* mep_expansion;     ///< kernel expansion to approximate
 	std::size_t m_approximatingVectors; ///< number of vectors in the basis
