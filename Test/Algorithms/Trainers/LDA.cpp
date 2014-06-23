724a96dde40195bf1ed8215d4b946b59ecb385d6
oswinkrause <Ulfgard@users.noreply.github.com>
2014-06-23 07:52:49 +0000

    added weighting for LDA. Only problem is, that i can't get the unbiased estimates right

--- Test/Algorithms/Trainers/LDA.cpp
+++ Test/Algorithms/Trainers/LDA.cpp
@@ -9,7 +9,7 @@
 using namespace shark;
 
 BOOST_AUTO_TEST_CASE( LDA_TEST_TWOCLASS ){
-	const size_t trainExamples = 12000;
+	const size_t TrainExamples = 12000;
 	LDA trainer;
 	LinearClassifier<> model;
 
@@ -35,11 +35,11 @@ BOOST_AUTO_TEST_CASE( LDA_TEST_TWOCLASS ){
 	MultiVariateNormalDistribution dist(covariance);
 
 
-	std::vector<RealVector> input(trainExamples,RealVector(2));
-	std::vector<unsigned int> target(trainExamples);
+	std::vector<RealVector> input(TrainExamples,RealVector(2));
+	std::vector<unsigned int> target(TrainExamples);
 
 	double statisticalBayesRisk=0;
-	for(size_t i=0;i!=trainExamples;++i){
+	for(size_t i=0;i!=TrainExamples;++i){
 		//create samples. class 1 has double as many elements as class 0
 		target[i]=(i%3 == 0);
 		input[i]=dist().first+mean[target[i]];
@@ -51,7 +51,7 @@ BOOST_AUTO_TEST_CASE( LDA_TEST_TWOCLASS ){
 		unsigned int bayesTarget = dist0>dist1;
 		statisticalBayesRisk+= bayesTarget != target[i];
 	}
-	statisticalBayesRisk/=trainExamples;
+	statisticalBayesRisk/=TrainExamples;
 
 	ClassificationDataset dataset = createLabeledDataFromRange(input,target);
 
@@ -70,7 +70,7 @@ BOOST_AUTO_TEST_CASE( LDA_TEST_TWOCLASS ){
 }
 
 BOOST_AUTO_TEST_CASE( LDA_TEST_TWOCLASS_SINGULAR ){
-	const size_t trainExamples = 12000;
+	const size_t TrainExamples = 12000;
 	LDA trainer;
 	LinearClassifier<> model;
 
@@ -99,11 +99,11 @@ BOOST_AUTO_TEST_CASE( LDA_TEST_TWOCLASS_SINGULAR ){
 	double prior[2]={std::log(2.0/3.0),std::log(1.0/3.0)};
 
 
-	std::vector<RealVector> input(trainExamples,RealVector(3));
-	std::vector<unsigned int> target(trainExamples);
+	std::vector<RealVector> input(TrainExamples,RealVector(3));
+	std::vector<unsigned int> target(TrainExamples);
 
 	double statisticalBayesRisk=0;
-	for(size_t i=0;i!=trainExamples;++i){
+	for(size_t i=0;i!=TrainExamples;++i){
 		//create sample
 		target[i]= (i%3 != 0);
 		RealVector vec = dist().first+mean[target[i]];
@@ -116,7 +116,7 @@ BOOST_AUTO_TEST_CASE( LDA_TEST_TWOCLASS_SINGULAR ){
 		statisticalBayesRisk+= bayesTarget != target[i];
 		init(input[i])<<vec,0;//add third zero
 	}
-	statisticalBayesRisk/=trainExamples;
+	statisticalBayesRisk/=TrainExamples;
 
 	ClassificationDataset dataset = createLabeledDataFromRange(input,target);
 
@@ -134,16 +134,83 @@ BOOST_AUTO_TEST_CASE( LDA_TEST_TWOCLASS_SINGULAR ){
 
 }
 
-BOOST_AUTO_TEST_CASE( LDA_TEST_MULTICLASS ){
-	const size_t trainExamples = 200000;
-	const unsigned int classes = 10;
-	LDA trainer;
-	LinearClassifier<> model;
-	Rng::seed(44);
+//~ BOOST_AUTO_TEST_CASE( LDA_TEST_MULTICLASS ){
+	//~ const size_t TrainExamples = 200000;
+	//~ const unsigned int classes = 10;
+	//~ LDA trainer;
+	//~ LinearClassifier<> model;
+	//~ Rng::seed(44);
 
+
+	//~ //create datatsets - overlapping normal distributions
+
+	//~ RealMatrix covariance(2,2);
+	//~ covariance(0,0)=16;
+	//~ covariance(0,1)=8;
+	//~ covariance(1,0)=8;
+	//~ covariance(1,1)=16;
+	//~ RealMatrix inverse(2,2,0.0);
+	//~ inverse(0,0) = inverse(1,1) = 1.0;
+	//~ blas::solveSymmSystemInPlace<blas::SolveAXB>(covariance,inverse);
+	
+	//~ std::vector<RealVector> mean(classes,RealVector(2));
+	//~ for(unsigned int c = 0; c != classes; ++c){
+		//~ for(std::size_t j = 0; j != 2; ++j){
+			//~ mean[c](j) = Rng::gauss(0,30);
+		//~ }
+	//~ }
+	//~ MultiVariateNormalDistribution dist(covariance);
+
+
+	//~ std::vector<RealVector> input(TrainExamples,RealVector(2));
+	//~ std::vector<unsigned int> target(TrainExamples);
+
+	//~ double statisticalBayesRisk=0;
+	//~ for(size_t i=0;i!=TrainExamples;++i){
+		//~ //create sample
+		//~ target[i]=i%classes;
+		//~ input[i]=dist().first+mean[target[i]];
+		//~ //calculate bayes Target - the best fit to the distributions
+		//~ unsigned int bayesTarget = 0;
+		//~ double minDist = 1.e30;
+		//~ for(unsigned int c = 0; c != classes; ++c){
+			//~ RealVector diff=input[i]-mean[c];
+			//~ double dist=inner_prod(diff,prod(inverse,diff));
+			//~ if(dist<minDist){
+				//~ minDist=dist;
+				//~ bayesTarget=c;
+			//~ }
+		//~ }
+		//~ statisticalBayesRisk+= bayesTarget != target[i];
+	//~ }
+	//~ statisticalBayesRisk/=TrainExamples;
+
+	//~ ClassificationDataset dataset = createLabeledDataFromRange(input,target);
+
+	//~ trainer.train(model, dataset);
+
+	//~ //double classificatorRisk=0;
+	//~ Data<unsigned int> results = model(dataset.inputs());
+	
+	//~ ZeroOneLoss<> loss;
+	//~ double classificatorRisk = loss.eval(dataset.labels(),results);
+	
+	//~ std::cout<<"statistical bayes Risk: "<<statisticalBayesRisk<<std::endl;
+	//~ std::cout<<"classificator Risk: "<<classificatorRisk<<std::endl;
+	//~ BOOST_CHECK_SMALL(classificatorRisk-statisticalBayesRisk,10e-2);
+
+//~ }
+
+
+
+BOOST_AUTO_TEST_CASE( LDA_TEST_MULTICLASS_WEIGHTING ){
+	const size_t TrainExamples = 100;
+	const size_t Trials = 10;
+	const size_t DatasetSize = 500;//size of the dataset after resampling
+	const unsigned int classes = 10;
+	
 
 	//create datatsets - overlapping normal distributions
-
 	RealMatrix covariance(2,2);
 	covariance(0,0)=16;
 	covariance(0,1)=8;
@@ -155,48 +222,68 @@ BOOST_AUTO_TEST_CASE( LDA_TEST_MULTICLASS ){
 	
 	std::vector<RealVector> mean(classes,RealVector(2));
 	for(unsigned int c = 0; c != classes; ++c){
-		for(std::size_t j = 0; j != 2; ++j){
-			mean[c](j) = Rng::gauss(0,30);
-		}
+		mean[c](0) = Rng::gauss(0,30);
+		mean[c](1) = Rng::gauss(0,30);
 	}
 	MultiVariateNormalDistribution dist(covariance);
 
+	std::vector<RealVector> input(TrainExamples,RealVector(2));
+	std::vector<unsigned int> target(TrainExamples);
 
-	std::vector<RealVector> input(trainExamples,RealVector(2));
-	std::vector<unsigned int> target(trainExamples);
-
-	double statisticalBayesRisk=0;
-	for(size_t i=0;i!=trainExamples;++i){
+	for(size_t i=0;i!=TrainExamples;++i){
 		//create sample
 		target[i]=i%classes;
 		input[i]=dist().first+mean[target[i]];
-		//calculate bayes Target - the best fit to the distributions
-		unsigned int bayesTarget = 0;
-		double minDist = 1.e30;
-		for(unsigned int c = 0; c != classes; ++c){
-			RealVector diff=input[i]-mean[c];
-			double dist=inner_prod(diff,prod(inverse,diff));
-			if(dist<minDist){
-				minDist=dist;
-				bayesTarget=c;
-			}
-		}
-		statisticalBayesRisk+= bayesTarget != target[i];
 	}
-	statisticalBayesRisk/=trainExamples;
-
 	ClassificationDataset dataset = createLabeledDataFromRange(input,target);
-
-	trainer.train(model, dataset);
-
-	//double classificatorRisk=0;
-	Data<unsigned int> results = model(dataset.inputs());
 	
-	ZeroOneLoss<> loss;
-	double classificatorRisk = loss.eval(dataset.labels(),results);
+	//first check that the weighted and unweighted version give the same results when all weights are equal
+	LDA trainer;
+	LinearClassifier<> modelUnweighted;
+	LinearClassifier<> modelWeighted;
 	
-	std::cout<<"statistical bayes Risk: "<<statisticalBayesRisk<<std::endl;
-	std::cout<<"classificator Risk: "<<classificatorRisk<<std::endl;
-	BOOST_CHECK_SMALL(classificatorRisk-statisticalBayesRisk,10e-2);
-
+	trainer.train(modelUnweighted, dataset);
+	trainer.train(modelWeighted, WeightedLabeledData<RealVector,unsigned int>(dataset,2.0));//two as 1 might hide errors as sqrt(1)=1
+	
+	//we need to correct for the fact that the unweighted versions normalizes with inputs-classes
+	//and the unnormalized only with sumOfWeights=inputs.
+	BOOST_CHECK_SMALL(
+		norm_frobenius(modelUnweighted.decisionFunction().matrix()/0.9-modelWeighted.decisionFunction().matrix()),
+		0.0001
+	);
+	RealVector normalizedOffset = (modelUnweighted.decisionFunction().offset()-blas::repeat(std::log(0.1),10))/0.9;
+	BOOST_CHECK_SMALL(
+		norm_2(normalizedOffset-modelWeighted.decisionFunction().offset()+blas::repeat(std::log(0.1),10)),
+		0.0001
+	);
+	
+	//resample the dataset by creating duplications. This must be the same as the normal
+	//dataset initialized with the correct multiplicities
+	for(std::size_t trial = 0; trial != Trials; ++trial){
+		//generate weighted and unweighted dataset
+		WeightedLabeledData<RealVector,unsigned int> weightedDataset(dataset,0.0);
+		ClassificationDataset unweightedDataset(1);
+		unweightedDataset.batch(0).input.resize(DatasetSize,inputDimension(dataset));
+		unweightedDataset.batch(0).label.resize(DatasetSize);
+		RealVector classWeight(classes,0);
+		for(std::size_t i = 0; i != DatasetSize; ++i){
+			std::size_t index = Rng::discrete(0,TrainExamples-1);
+			weightedDataset.element(index).weight +=1.0;
+			unweightedDataset.element(i) = dataset.element(index);
+			classWeight(weightedDataset.element(index).data.label) += 1.0/DatasetSize;
+		}
+		trainer.train(modelUnweighted, unweightedDataset);
+		trainer.train(modelWeighted, weightedDataset);
+		double covarianceCorrection = double(DatasetSize-classes)/DatasetSize;
+		BOOST_CHECK_SMALL(
+			norm_frobenius(modelUnweighted.decisionFunction().matrix()/covarianceCorrection-modelWeighted.decisionFunction().matrix()),
+			0.0001
+		);
+		RealVector normalizedOffset = (modelUnweighted.decisionFunction().offset()-log(classWeight))/covarianceCorrection;
+		BOOST_CHECK_SMALL(
+			norm_2(normalizedOffset-modelWeighted.decisionFunction().offset()+log(classWeight)),
+			0.0001
+		);
+		
+	}
 }
