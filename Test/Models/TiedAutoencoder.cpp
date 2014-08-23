7ccc03ded92d87fe4e11e320cf2d195d7ed06bb8
oswinkrause <Ulfgard@users.noreply.github.com>
2014-08-23 06:52:01 +0000

    added TiedAutoencoder (autoencoder with tied weights)

---
+++ Test/Models/TiedAutoencoder.cpp
@@ -0,0 +1,101 @@
+#define BOOST_TEST_MODULE MODE_TIEDAUTOENCODER
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+#include "derivativeTestHelper.h"
+
+#include <shark/Models/TiedAutoencoder.h>
+#include <sstream>
+#include <boost/archive/polymorphic_text_iarchive.hpp>
+#include <boost/archive/polymorphic_text_oarchive.hpp>
+#include <shark/Rng/GlobalRng.h>
+
+using namespace std;
+using namespace boost::archive;
+using namespace shark;
+
+//check that the structure is correct, i.e. matrice have the right form and setting parameters works
+BOOST_AUTO_TEST_CASE( TIED_AUTOENCODER_Structure)
+{
+	std::size_t weightNum = 2*3+5;
+	TiedAutoencoder<LogisticNeuron,LogisticNeuron> net;
+	net.setStructure(2,3);
+	BOOST_REQUIRE_EQUAL(net.hiddenBias().size(),3u);
+	BOOST_REQUIRE_EQUAL(net.outputBias().size(),2u);
+	BOOST_CHECK_EQUAL(net.weightMatrix().size1(), 3u);
+	BOOST_CHECK_EQUAL(net.weightMatrix().size2(), 2u);
+	BOOST_REQUIRE_EQUAL(net.numberOfParameters(),weightNum);
+	
+	RealVector newParams(weightNum);
+	for(std::size_t i = 0; i != weightNum; ++i){
+		newParams(i) = Rng::uni(0,1);
+	}
+	//check that setting and getting parameters works
+	net.setParameterVector(newParams);
+	RealVector params = net.parameterVector();
+	BOOST_REQUIRE_EQUAL(params.size(),newParams.size());
+	for(std::size_t i = 0; i != weightNum; ++i){
+		BOOST_CHECK_CLOSE(newParams(i),params(i), 1.e-10);
+	}
+	//check that the weight matrix has the right values
+	std::size_t param = 0;
+	for(std::size_t i = 0; i != net.weightMatrix().size1(); ++i){
+		for(std::size_t j = 0; j != net.weightMatrix().size2(); ++j,++param){
+			BOOST_CHECK_EQUAL(net.weightMatrix()(i,j), newParams(param));
+		}
+	}
+	for(std::size_t i = 0; i != 3; ++i,++param){
+		BOOST_CHECK_EQUAL(net.hiddenBias()(i), newParams(param));
+	}
+	BOOST_CHECK_EQUAL(net.outputBias()(0), newParams(param));
+	BOOST_CHECK_EQUAL(net.outputBias()(1), newParams(param+1));
+}
+
+BOOST_AUTO_TEST_CASE( TIED_AUTOENCODER_Value )
+{
+	TiedAutoencoder<LogisticNeuron,LinearNeuron> net;
+	net.setStructure(3,2);
+	std::size_t numParams = 3*2+5;
+	
+	for(std::size_t i = 0; i != 100; ++i){
+		//initialize parameters
+		RealVector parameters(numParams);
+		for(size_t j=0; j != numParams;++j)
+			parameters(j)=Rng::gauss(0,1);
+		net.setParameterVector(parameters);
+
+		//the testpoints
+		RealVector point(3);
+		point(0)=Rng::uni(-5,5);
+		point(1)= Rng::uni(-5,5);
+		point(2)= Rng::uni(-5,5);
+
+		//evaluate ground truth result
+		RealVector hidden = sigmoid(prod(net.weightMatrix(),point)+net.hiddenBias());
+		RealVector output = prod(trans(net.weightMatrix()),hidden)+net.outputBias();
+		
+		//check whether final result is correct
+		RealVector netResult = net(point);
+		BOOST_CHECK_SMALL(netResult(0)-output(0),1.e-12);
+		BOOST_CHECK_SMALL(netResult(1)-output(1),1.e-12);
+		BOOST_CHECK_SMALL(netResult(2)-output(2),1.e-12);
+	}
+	
+	//now also test batches
+	RealMatrix inputs(100,3);
+	for(std::size_t i = 0; i != 100; ++i){
+		inputs(i,0)=Rng::uni(-5,5);
+		inputs(i,1)= Rng::uni(-5,5);
+		inputs(i,2)= Rng::uni(-5,5);
+	}
+	testBatchEval(net,inputs);
+}
+
+BOOST_AUTO_TEST_CASE( TIED_AUTOENCODER_WeightedDerivatives)
+{
+	TiedAutoencoder<LogisticNeuron,TanhNeuron> net;
+	net.setStructure(2,5);
+
+	testWeightedInputDerivative(net,1000,5.e-6,1.e-7);
+	testWeightedDerivative(net,1000,5.e-6,1.e-7);
+	testWeightedDerivativesSame(net,1000);
+}
---
+++ include/shark/Models/TiedAutoencoder.h
@@ -0,0 +1,292 @@
+/*!
+ * \brief   
+ * 
+ * \author      O. Krause
+ * \date        2010-2014
+ *
+ *
+ * \par Copyright 1995-2014 Shark Development Team
+ * 
+ * <BR><HR>
+ * This file is part of Shark.
+ * <http://image.diku.dk/shark/>
+ * 
+ * Shark is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Shark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Shark.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef SHARK_MODELS_TIEDAUTOENCODER_H
+#define SHARK_MODELS_TIEDAUTOENCODER_H
+
+#include <shark/Models/AbstractModel.h>
+#include <shark/Models/Neurons.h>
+#include <boost/serialization/vector.hpp>
+
+namespace shark{
+
+template<class HiddenNeuron,class OutputNeuron>
+class TiedAutoencoder :public AbstractModel<RealVector,RealVector>
+{
+	struct InternalState: public State{
+		RealMatrix hiddenResponses;
+		RealMatrix outputResponses;
+	};
+	
+
+public:
+	TiedAutoencoder(){
+		m_features|=HAS_FIRST_PARAMETER_DERIVATIVE;
+		m_features|=HAS_FIRST_INPUT_DERIVATIVE;
+	}
+
+	//! \brief From INameable: return the class name.
+	std::string name() const{ 
+		return "TiedAutoencoder";
+	}
+
+	//! \brief Number of input neurons.
+	std::size_t inputSize()const{
+		return m_weightMatrix.size2();
+	}
+	//! \brief Number of output neurons.
+	std::size_t outputSize()const{
+		return m_weightMatrix.size2();
+	}
+	
+	//! \brief Total number of hidden neurons.
+	std::size_t numberOfHiddenNeurons()const{
+		return m_weightMatrix.size1();
+	}
+	RealMatrix const& weightMatrix()const{
+		return m_weightMatrix;
+	}
+	RealVector const& hiddenBias()const{
+		return m_hiddenBias;
+	}
+	
+	RealVector const& outputBias()const{
+		return m_outputBias;
+	}
+	
+	RealMatrix& weightMatrix(){
+		return m_weightMatrix;
+	}
+	RealVector& hiddenBias(){
+		return m_hiddenBias;
+	}
+	
+	RealVector& outputBias(){
+		return m_outputBias;
+	}
+	
+	//! \brief Returns the total number of parameters of the network. 
+	std::size_t numberOfParameters()const{
+		return inputSize()*numberOfHiddenNeurons()+inputSize()+numberOfHiddenNeurons();
+	}
+
+	//! returns the vector of used parameters inside the weight matrix
+	RealVector parameterVector() const{
+		RealVector parameters(numberOfParameters());
+		init(parameters) << toVector(m_weightMatrix),m_hiddenBias,m_outputBias;
+		return parameters;
+	}
+	//! uses the values inside the parametervector to set the used values inside the weight matrix
+	void setParameterVector(RealVector const& newParameters){
+		SIZE_CHECK(newParameters.size() == numberOfParameters());
+		init(newParameters) >> toVector(m_weightMatrix),m_hiddenBias,m_outputBias;
+	}
+
+	//! \brief Returns the output of all neurons after the last call of eval
+	//!
+	//!     \param  state last result of eval
+	//!     \return Output value of the neurons.
+	RealMatrix const& neuronResponses(State const& state)const{
+		InternalState const& s = state.toState<InternalState>();
+		return s.responses;
+	}
+	
+	boost::shared_ptr<State> createState()const{
+		return boost::shared_ptr<State>(new InternalState());
+	}
+
+	void evalLayer(std::size_t layer,RealMatrix const& patterns,RealMatrix& outputs)const{
+		SIZE_CHECK(layer < 2);
+		std::size_t numPatterns = patterns.size1();
+		
+		if(layer == 0){//input->hidden
+			SIZE_CHECK(patterns.size2() == weightMatrix().size2());
+			std::size_t numOutputs = weightMatrix().size1();
+			outputs.resize(numPatterns,numOutputs);
+			outputs.clear();
+			axpy_prod(patterns,trans(weightMatrix()),outputs);
+			noalias(outputs) += repeat(hiddenBias(),numPatterns);
+			noalias(outputs) = m_hiddenNeuron(outputs);
+		}
+		else{//hidden->output
+			SIZE_CHECK(patterns.size2() == weightMatrix().size1());
+			std::size_t numOutputs = weightMatrix().size2();
+			outputs.resize(numPatterns,numOutputs);
+			outputs.clear();
+			axpy_prod(patterns,weightMatrix(),outputs);
+			noalias(outputs) += repeat(outputBias(),numPatterns);
+			noalias(outputs) = m_outputNeuron(outputs);
+		}
+	}
+	
+	///\brief Returns the response of the i-th layer given the input of that layer.
+	///
+	/// this is usefull if only a portion of the network needs to be evaluated
+	/// be aware that this only works without shortcuts in the network
+	Data<RealVector> evalLayer(std::size_t layer, Data<RealVector> const& patterns)const{
+		SIZE_CHECK(layer < 2);
+		int batches = (int) patterns.numberOfBatches();
+		Data<RealVector> result(batches);
+		SHARK_PARALLEL_FOR(int i = 0; i < batches; ++i){
+			evalLayer(layer,patterns.batch(i),result.batch(i));
+		}
+		return result;
+	}
+	
+	void eval(RealMatrix const& patterns,RealMatrix& output, State& state)const{
+		InternalState& s = state.toState<InternalState>();
+		evalLayer(0,patterns,s.hiddenResponses);//propagate input->hidden
+		evalLayer(1,s.hiddenResponses,s.outputResponses);//propagate hidden->output
+		output = s.outputResponses;
+	}
+	using AbstractModel<RealVector,RealVector>::eval;
+
+	void weightedParameterDerivative(
+		BatchInputType const& patterns, RealMatrix const& coefficients, State const& state, RealVector& gradient
+	)const{
+		SIZE_CHECK(coefficients.size2() == outputSize());
+		SIZE_CHECK(coefficients.size1() == patterns.size1());
+		
+		RealMatrix outputDelta = coefficients;
+		RealMatrix hiddenDelta;
+		computeDelta(state,outputDelta,hiddenDelta);
+		computeParameterDerivative(patterns,outputDelta,hiddenDelta,state,gradient);
+	}
+	
+	void weightedInputDerivative(
+		BatchInputType const& patterns, RealMatrix const& coefficients, State const& state, BatchInputType& inputDerivative
+	)const{
+		SIZE_CHECK(coefficients.size2() == outputSize());
+		SIZE_CHECK(coefficients.size1() == patterns.size1());
+		
+		RealMatrix outputDelta = coefficients;
+		RealMatrix hiddenDelta;
+		computeDelta(state,outputDelta,hiddenDelta,inputDerivative);
+	}
+	
+	virtual void weightedDerivatives(
+		BatchInputType const & patterns,
+		BatchOutputType const & coefficients,
+		State const& state,
+		RealVector& parameterDerivative,
+		BatchInputType& inputDerivative
+	)const{
+		SIZE_CHECK(coefficients.size2() == outputSize());
+		SIZE_CHECK(coefficients.size1() == patterns.size1());
+
+		RealMatrix outputDelta = coefficients;
+		RealMatrix hiddenDelta;
+		computeDelta(state,outputDelta,hiddenDelta,inputDerivative);
+		computeParameterDerivative(patterns,outputDelta,hiddenDelta,state,parameterDerivative);
+	}
+	
+	void setStructure(
+		std::size_t in,std::size_t hidden
+	){
+		m_weightMatrix.resize(hidden,in);
+		m_hiddenBias.resize(hidden);
+		m_outputBias.resize(in);
+	}
+	
+	//! From ISerializable, reads a model from an archive
+	void read( InArchive & archive ){
+		archive>>m_weightMatrix;
+		archive>>m_hiddenBias;
+		archive>>m_outputBias;
+	}
+
+	//! From ISerializable, writes a model to an archive
+	void write( OutArchive & archive ) const{
+		archive<<m_weightMatrix;
+		archive<<m_hiddenBias;
+		archive<<m_outputBias;
+	}
+
+
+private:
+	
+	void computeDelta(
+		State const& state, RealMatrix& outputDelta, RealMatrix& hiddenDelta
+	)const{
+		InternalState const& s = state.toState<InternalState>();
+
+		noalias(outputDelta) *= m_outputNeuron.derivative(s.outputResponses);
+		hiddenDelta.resize(outputDelta.size1(),numberOfHiddenNeurons());
+		axpy_prod(outputDelta,trans(weightMatrix()),hiddenDelta,true);
+		noalias(hiddenDelta) *= m_hiddenNeuron.derivative(s.hiddenResponses);
+	}
+	
+	void computeDelta(
+		State const& state, RealMatrix& outputDelta, RealMatrix& hiddenDelta, RealMatrix& inputDelta
+	)const{
+		computeDelta(state,outputDelta,hiddenDelta);
+		inputDelta.resize(outputDelta.size1(),inputSize());
+		axpy_prod(hiddenDelta,weightMatrix(),inputDelta,true);
+	}
+	
+	void computeParameterDerivative(
+		RealMatrix const& patterns, RealMatrix const& outputDelta, RealMatrix const& hiddenDelta,
+		State const& state, RealVector& gradient
+	)const{
+		InternalState const& s = state.toState<InternalState>();
+		std::size_t hiddenParams = inputSize()*numberOfHiddenNeurons();
+		std::size_t numHidden = numberOfHiddenNeurons();
+		gradient.resize(numberOfParameters());
+		gradient.clear();
+		axpy_prod(
+			trans(s.hiddenResponses),
+			outputDelta,
+			to_matrix(subrange(gradient,0,hiddenParams),numHidden,inputSize()),false
+		);
+		axpy_prod(
+			trans(hiddenDelta),
+			patterns,
+			to_matrix(subrange(gradient,0,hiddenParams),numHidden,inputSize()),false
+		);
+		
+		std::size_t hiddenBiasPos = hiddenParams;
+		std::size_t outputBiasPos = hiddenBiasPos+numHidden;
+		subrange(gradient,hiddenBiasPos,outputBiasPos) = sum_rows(hiddenDelta);
+		subrange(gradient,outputBiasPos,outputBiasPos+inputSize()) = sum_rows(outputDelta);
+	}
+
+	//! weight matrix between input and hidden layer. the transpose of this is used to connect hidden->output.
+	RealMatrix m_weightMatrix;
+	//! bias weights of the hidden neurons
+	RealVector m_hiddenBias;
+	//! bias weights of the visible neurons
+	RealVector m_outputBias;
+
+	//!Type of hidden neuron. See Models/Neurons.h for a few choices
+	HiddenNeuron m_hiddenNeuron;
+	//! Type of output neuron. See Models/Neurons.h for a few choices
+	OutputNeuron m_outputNeuron;
+};
+
+
+}
+#endif
