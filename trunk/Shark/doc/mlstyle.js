866ed3b9b9bae4339d4e116695af67c66a8828cb
m-p-t <m-p-t@f06a09e3-3943-0410-8714-a4bb87ba143f>
2014-09-04 22:43:14 +0000

    - repaired previous hacks for vector redefinition in mathjax for strictly no absolute paths (e.g. allowing copy to diku server with docs built elsewhere). seems to work

---
+++ trunk/Shark/doc/mlstyle.js
@@ -0,0 +1,10 @@
+MathJax.Hub.Config({
+  TeX: {
+    Macros: {
+      vec: ["{\\bf #1}",1]
+    }
+  }
+});
+
+MathJax.Hub.Configured();
+//nicked from https://groups.google.com/forum/#!msg/mathjax-users/J-36V22-G9Q/AW3ncCbJzS8J
