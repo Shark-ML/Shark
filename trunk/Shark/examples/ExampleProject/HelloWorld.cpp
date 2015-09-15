bb9cd4cc72be7adcadca0e59afddbabca78c4ea0
oswinkrause <oswinkrause@f06a09e3-3943-0410-8714-a4bb87ba143f>
2015-09-15 08:03:50 +0000

    added new example project

---
+++ trunk/Shark/examples/ExampleProject/HelloWorld.cpp
@@ -0,0 +1,41 @@
+//===========================================================================
+/*!
+ * 
+ *
+ * \brief       Example program for hierarchical clustering.
+ * 
+ * 
+ *
+ * \author      T. Glasmachers
+ * \date        2011
+ *
+ *
+ * \par Copyright 1995-2015 Shark Development Team
+ * 
+ * <BR><HR>
+ * This file is part of Shark.
+ * <http://image.diku.dk/shark/>
+ * 
+ * Shark is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Shark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Shark.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+//===========================================================================
+
+#include <shark/Core/Shark.h>
+#include <iostream>
+
+int main( int argc, char ** argv ) 
+{
+	shark::Shark::info( std::cout );
+}
