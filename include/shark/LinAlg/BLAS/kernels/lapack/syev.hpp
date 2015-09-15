6f255622db9ea023d40891957a81ae9644ea4052
oswinkrause <Ulfgard@users.noreply.github.com>
2015-09-15 07:19:09 +0000

    fixed lapack issue and deactivated ScopedHandleTest

--- include/shark/LinAlg/BLAS/kernels/lapack/syev.hpp
+++ include/shark/LinAlg/BLAS/kernels/lapack/syev.hpp
@@ -37,13 +37,13 @@
 
 #define SHARK_LAPACK_DSYEV FORTRAN_ID(dsyev)
 
-//extern "C"{
-//void SHARK_LAPACK_DSYEV( 
-//	const char* jobz, const char* uplo, const int *n,
-//	double* a, const int * lda, double* w,
-//	double* work, const int * lwork, int* info
-//);
-//}
+extern "C"{
+void SHARK_LAPACK_DSYEV( 
+	const char* jobz, const char* uplo, const int *n,
+	double* a, const int * lda, double* w,
+	double* work, const int * lwork, int* info
+);
+}
 
 
 
