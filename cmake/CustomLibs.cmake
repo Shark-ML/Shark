c19d3211c3d1009dccbf1bc1ba73626116bd4fa0
skn123 <skn123@gmail.com>
2015-11-04 23:47:02 +0530

    Cmake files

---
+++ cmake/CustomLibs.cmake
@@ -0,0 +1,15 @@
+SET(NonModularLibrary_hdf5 ON CACHE BOOL "Use hdf5 Library" FORCE)
+SET(NonModularLibrary_openblas ON CACHE BOOL "Use OpenBlas Library" FORCE)
+ADD_DEFINITIONS(-DSHARK_USE_OPENBLAS)
+SET(MODULAR_MANDATORY_LIB_LIST boost-1_55)
+SET(BOOST_MANDATORY_COMPONENTS
+    thread
+    system
+    date_time
+    program_options 
+    serialization
+    filesystem
+	  unit_test_framework
+)
+FIND_PACKAGE(ThirdPartyLibraries_VS2013 REQUIRED)
+list(APPEND LINK_LIBRARIES ${EXT_LIBS}) 
