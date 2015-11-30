e777f1c9b0e165c4477fa5aba9f98d1a11dd2db6
skn123 <skn123@gmail.com>
2015-12-01 01:15:41 +0530

    opeblas integrated

--- cmake/CustomLibs.cmake
+++ cmake/CustomLibs.cmake
@@ -1,7 +1,7 @@
 SET(NonModularLibrary_hdf5 ON CACHE BOOL "Use hdf5 Library" FORCE)
 SET(NonModularLibrary_openblas ON CACHE BOOL "Use OpenBlas Library" FORCE)
 ADD_DEFINITIONS(-DSHARK_USE_OPENBLAS)
-SET(MODULAR_MANDATORY_LIB_LIST boost-1_55)
+SET(MODULAR_MANDATORY_LIB_LIST boost-1_59)
 SET(BOOST_MANDATORY_COMPONENTS
     thread
     system
@@ -11,5 +11,5 @@ SET(BOOST_MANDATORY_COMPONENTS
     filesystem
 	  unit_test_framework
 )
-FIND_PACKAGE(ThirdPartyLibraries_VS2013 REQUIRED)
+FIND_PACKAGE(ThirdPartyLibraries_VS2015 REQUIRED)
 list(APPEND LINK_LIBRARIES ${EXT_LIBS}) 
