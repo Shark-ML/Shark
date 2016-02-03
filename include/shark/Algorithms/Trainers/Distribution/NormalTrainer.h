fa39fe793c963ea4b84a538876a0d6ecc9f6cb12
Oswin <oswin.krause@di.ku.dk>
2016-02-03 13:31:36 +0100

    fixed missing include

--- include/shark/Algorithms/Trainers/Distribution/NormalTrainer.h
+++ include/shark/Algorithms/Trainers/Distribution/NormalTrainer.h
@@ -43,6 +43,7 @@
 #include <boost/accumulators/statistics/stats.hpp>
 #include <boost/accumulators/statistics/variance.hpp>
 #include <boost/bind/bind.hpp>
+#include <boost/bind/placeholders.hpp>
 #include <boost/range/algorithm/for_each.hpp>
 
 namespace shark {
