99f446a47a0d6eedc4559786f1ad39dc8ea8b34d
Tobias Glasmachers <tobias.glasmachers@ini.rub.de>
2016-03-29 16:46:45 +0200

    minor improvement

--- include/shark/Algorithms/DirectSearch/SweepingNonDominatedSort.h
+++ include/shark/Algorithms/DirectSearch/SweepingNonDominatedSort.h
@@ -1,40 +1,41 @@
-/*!
- * \brief       Implements the Improved Run-Time Complexity Algorithm for Non-Dominated Sorting.
- * 
- * \author      T. Glasmachers
- * \date        2016
- *
- *
- * \par Copyright 1995-2016 Shark Development Team
- * 
- * <BR><HR>
- * This file is part of Shark.
- * <http://image.diku.dk/shark/>
- * 
- * Shark is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published 
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * Shark is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with Shark.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
+///
+/// \brief       Implements the Improved Run-Time Complexity Algorithm for Non-Dominated Sorting.
+/// 
+/// \author      T. Glasmachers
+/// \date        2016
+///
+///
+/// \par Copyright 1995-2016 Shark Development Team
+/// 
+/// <BR><HR>
+/// This file is part of Shark.
+/// <http://image.diku.dk/shark/>
+/// 
+/// Shark is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Lesser General Public License as published 
+/// by the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+/// 
+/// Shark is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU Lesser General Public License for more details.
+/// 
+/// You should have received a copy of the GNU Lesser General Public License
+/// along with Shark.  If not, see <http://www.gnu.org/licenses/>.
+///
+
 #ifndef SHARK_ALGORITHMS_DIRECTSEARCH_SWEEPINGNONDOMINATEDSORT_H
 #define SHARK_ALGORITHMS_DIRECTSEARCH_SWEEPINGNONDOMINATEDSORT_H
 
 #include <shark/Algorithms/DirectSearch/ParetoDominance.h>
 #include <shark/Algorithms/DirectSearch/FitnessExtractor.h>
 #include <shark/LinAlg/Base.h>
-#include <boost/bimap.hpp>
+//#include <boost/bimap.hpp>
 #include <vector>
 #include <list>
 #include <set>
+#include <map>
 #include <utility>
 #include <algorithm>
 
@@ -42,23 +43,23 @@
 namespace shark {
 
 
-/**
- * \brief Implements the Improved Run-Time Complexity Algorithm for Non-Dominated Sorting.
- *
- * Assembles subsets/fronts of mutually non-dominated individuals.
- * Afterwards every individual is assigned a rank by pop[i].rank() = frontIndex.
- * The front of non-dominated points has the value 1.
- * 
- * The algorithm is described in:
- * Generalizing the Improved Run-Time Complexity Algorithm for Non-Dominated Sorting.
- * Félix-Antoine Fortin, Simon Grenier, and Marc Parizeau,
- * Proceedings Genetic and Evolutionary Computation Conference (GECCO), pp. 615-622, 2013.
- *
- * The algorithm is based on an earlier publication by Jensen, 2003. Its runtime
- * complexity for n points and m objectives is \f$ \mathcal{O}(n \log(n)^m) \f$,
- * which is often better than the complexity \f$ \mathcal{O}(n^2 m) \f$ of the
- * classic "fast non-dominated sorting" algorithm.
- */
+///
+/// \brief Implements the Improved Run-Time Complexity Algorithm for Non-Dominated Sorting.
+///
+/// Assembles subsets/fronts of mutually non-dominated individuals.
+/// Afterwards every individual is assigned a rank by pop[i].rank() = frontIndex.
+/// The front of non-dominated points has the value 1.
+/// 
+/// The algorithm is described in:
+/// Generalizing the Improved Run-Time Complexity Algorithm for Non-Dominated Sorting.
+/// Félix-Antoine Fortin, Simon Grenier, and Marc Parizeau,
+/// Proceedings Genetic and Evolutionary Computation Conference (GECCO), pp. 615-622, 2013.
+///
+/// The algorithm is based on an earlier publication by Jensen, 2003. Its runtime
+/// complexity for n points and m objectives is \f$ \mathcal{O}(n \log(n)^m) \f$,
+/// which is often better than the complexity \f$ \mathcal{O}(n^2 m) \f$ of the
+/// classic "fast non-dominated sorting" algorithm.
+///
 template <class Extractor>
 class BaseSweepingNonDominatedSort
 {
@@ -100,18 +101,11 @@ private:
 
 	typedef std::vector<Point*> ContainerType;
 
-	std::vector<double> y;
-
 public:
 	/// \brief Executes the non-dominated sorting algorithm.
 	///
 	/// Afterwards every individual is assigned a rank by pop[i].rank() = frontNumber.
 	/// The front of dominating points has the value 1.
-	///
-	/// Note: This function is not thread safe. Don't use the same
-	/// sorter object in multiple threads simultaneously. It is
-	/// perfectly safe if one sorter is used per sorting operation,
-	/// which is anyway to preferred mode of operation.
 	///
 	/// \param pop [in,out] Population to subdivide into fronts of non-dominated individuals.
 	///
@@ -125,8 +119,6 @@ public:
 		// prepare set of unique objective vectors
 		std::set<Point, LexicographicOrder> unique(comp);
 		for (auto const& ind : pop) unique.insert(Point(e(ind)));
-
-		y.resize(unique.size() + 1);
 
 		// unroll the set into a vector
 		std::vector<Point> points(unique.size());
@@ -149,8 +141,6 @@ public:
 			SHARK_ASSERT(it != points.end());
 			ind.rank() = it->frt;
 		}
-
-		y.clear();
 	}
 
 private:
@@ -234,23 +224,20 @@ private:
 	// which is nearly always the case in practice.
 	void sweepA(ContainerType& S)
 	{
-		std::list<const Point*> T;
-		T.push_back(S[0]);
+		std::map<std::size_t, double> T;
+		T[S[0]->frt] = S[0]->obj[1];
 		for (std::size_t i=1; i<S.size(); i++)
 		{
+			// O(T.size()) operation, should be logarithmic...
 			std::size_t r = 0;
 			for (auto p : T)
 			{
-				if (p->obj[1] <= S[i]->obj[1]) r = std::max(r, p->frt);
+				if (p.second <= S[i]->obj[1]) r = std::max(r, p.first);
 			}
+
 			if (r > 0) S[i]->frt = std::max(S[i]->frt, r + 1);
-			for (auto it=T.begin(); it != T.end(); )
-			{
-				auto next = it; ++next;
-				if ((*it)->frt == S[i]->frt) T.erase(it);
-				it = next;
-			}
-			T.push_back(S[i]);
+
+			T[S[i]->frt] = S[i]->obj[1];
 		}
 	}
 
@@ -391,7 +378,7 @@ private:
 				i++;
 			}
 
-			// linear loop - here we need a log(T.size()) lookup!
+			// O(T.size()) operation, should be logarithmic...
 			std::size_t r = 0;
 			for (auto p : T)
 			{
