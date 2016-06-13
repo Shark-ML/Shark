44d7c0d950488e9e508b852702b3533305d3bf72
Oswin Krause <oswin.krause@di.ku.dk>
2016-06-13 09:52:54 +0200

    removed a number of non-compiling files that aparently no-one ever tried to use.

--- include/shark/Models/Kernels/GaussianRbfKernel.h
+++ include/shark/Models/Kernels/GaussianRbfKernel.h
@@ -104,12 +104,12 @@ public:
 	}
 
 	/// Get the bandwidth parameter value.
-	inline double gamma() const {
+	double gamma() const {
 		return m_gamma;
 	}
 
 	/// Return ``standard deviation'' of Gaussian.
-	inline double sigma() const{ 
+	double sigma() const{ 
 		return 1. / std::sqrt(2 * m_gamma); 
 	}
 
@@ -121,8 +121,8 @@ public:
 	}
 
 	/// Set ``standard deviation'' of Gaussian.
-	inline double setSigma(double sigma) const{ 
-		return m_gamma = 1. / (2 * sigma * sigma); 
+	void setSigma(double sigma){ 
+		m_gamma = 1. / (2 * sigma * sigma); 
 	}
 
 	/// From ISerializable.
