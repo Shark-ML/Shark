6881303f6ca8bc1079a03ba161d4628b548482ca
Oswin Krause <oswin.krause@di.ku.dk>
2016-07-11 11:58:20 +0200

    improved SAG trainer in the sparse case

--- include/shark/Algorithms/Trainers/LinearSAGTrainer.h
+++ include/shark/Algorithms/Trainers/LinearSAGTrainer.h
@@ -196,9 +196,11 @@ private:
 		iterate(model,dataset,loss);
 	}
 	
+	//dense vector case is easier, mostly implemented for simple exposition of the algorithm
+	template<class T>
 	void iterate(
-		LinearModel<InputType>& model,
-		WeightedLabeledData<InputType, LabelType> const& dataset,
+		LinearModel<blas::vector<T> >& model,
+		WeightedLabeledData<blas::vector<T>, LabelType> const& dataset,
 		AbstractLoss<LabelType,RealVector> const& loss
 	){
 		
@@ -244,25 +246,126 @@ private:
 			// compute loss gradient
 			double currentValue = loss.evalDerivative(data[b].label, f_b, derivative);
 			
-			//update gradient
+			//update gradient (needs to be multiplied with kappa)
 			noalias(grad) += probabilities(b) * outer_prod(derivative-column(gradD,b), data[b].input);
 			if(m_offset) noalias(gradOffset) += probabilities(b) *(derivative-column(gradD,b));
 			noalias(column(gradD,b)) = derivative; //we got a new estimate for D of element b.
 			
 			// update gradient
-			double eta = 1/(L+m_lambda);
+			double eta = 1.0/(L+m_lambda);
 			noalias(model.matrix()) *= 1 - eta * m_lambda;//2-norm regularization
-			noalias(model.matrix()) -= eta * grad;
+			for(std::size_t i = 0; i != labelDim; ++i){
+				for(std::size_t j = 0; j != dim; ++j){
+					model.matrix()(i,j) -= eta*grad(i,j);
+				}
+			}
+			//~ noalias(model.matrix()) -= eta * grad;
 			if(m_offset) noalias(model.offset()) -= eta * gradOffset;
 			
 			//line-search procedure, 4.6 in the paper
 			noalias(f_b) -= derivative/L*pointNorms(b);
 			double newValue = loss.eval(data[b].label, f_b);
-			if(newValue > currentValue - 1/(2*L)*norm_sqr(derivative)*pointNorms(b)){
+			if(norm_sqr(derivative)*pointNorms(b) > 1.e-8 && newValue > currentValue - 1/(2*L)*norm_sqr(derivative)*pointNorms(b)){
+				L *= 2;
+			}
+			L*= std::pow(2.0,-1.0/ell);//allow L to slightly shrink in case our initial estimate was too large
+		}
+	}
+	
+	template<class T>
+	void iterate(
+		LinearModel<blas::compressed_vector<T> >& model,
+		WeightedLabeledData<blas::compressed_vector<T>, LabelType> const& dataset,
+		AbstractLoss<LabelType,RealVector> const& loss
+	){
+		
+		//get stats of the dataset
+		DataView<LabeledData<InputType, LabelType> const> data(dataset.data());
+		std::size_t ell = data.size();
+		std::size_t labelDim = model.outputSize();
+		std::size_t dim = model.inputSize();
+		
+		//set number of iterations
+		std::size_t iterations = m_maxEpochs * ell;
+		if(m_maxEpochs == 0)
+			iterations = std::max(10 * ell, std::size_t(std::ceil(dim * ell)));
+		
+		//picking distribution picks proportional to weight
+		RealVector probabilities = createBatch(dataset.weights().elements());
+		probabilities /= sum(probabilities);
+		MultiNomialDistribution dist(probabilities);
+			
+		//variables used for the SAG loop
+		RealMatrix gradD(labelDim,ell,0); // gradients of regularized loss minimization with a linear model have the form sum_i D_i*x_i. We store the last acquired estimate
+		RealMatrix grad(labelDim,dim);// gradient of the weight matrix.
+		RealVector gradOffset(labelDim,0); //sum_i D_i, gradient estimate for the offset
+		RealVector pointNorms(ell); //norm of each point in the dataset
+		for(std::size_t  i = 0; i != ell; ++i){
+			pointNorms(i) = norm_sqr(data[i].input);
+		}
+		// preinitialize everything to prevent costly memory allocations in the loop
+		RealVector f_b(labelDim, 0.0); // prediction of the model
+		RealVector derivative(labelDim, 0.0); //derivative of the loss
+		double kappa =1; //we store the matrix as kappa*model.matrix() where kappa stores the effect of the 2-norm regularisation
+		double L = 1; // initial estimate for the lipschitz-constant
+		
+		//just in time updating for sparse inputs.
+		//we need to store a running sum of step lengths which we can then apply whenever an index got updated
+		RealVector appliedRates(dim,0.0);//applied steps since the last reset for each dimension
+		double stepsCumSum = 0.0;// cumulative update steps
+		
+		
+		// SAG loop
+		for(std::size_t iter = 0; iter < iterations; iter++)
+		{
+			// choose data point
+			std::size_t b = dist(Rng::globalRng);
+			auto point = data[b];
+			
+			//just in time update of the previous steps for every nonzero element of point b
+			auto end = point.input.end();
+			for(auto  pos = point.input.begin(); pos != end; ++pos){
+				std::size_t index = pos.index();
+				noalias(column(model.matrix(),index)) -= (stepsCumSum - blas::repeat(appliedRates(index),labelDim))*column(grad,index);
+				appliedRates(index) = stepsCumSum;
+			}
+			
+			// compute prediction
+			noalias(f_b) = kappa * prod(model.matrix(), point.input);
+			if(m_offset) noalias(f_b) += model.offset();
+			
+			// compute loss gradient
+			double currentValue = loss.evalDerivative(point.label, f_b, derivative);
+			
+			//update gradient (needs to be multiplied with kappa)
+			noalias(grad) += probabilities(b) * outer_prod(derivative-column(gradD,b), point.input);
+			if(m_offset) noalias(gradOffset) += probabilities(b) *(derivative-column(gradD,b));
+			noalias(column(gradD,b)) = derivative; //we got a new estimate for D of element b.
+			
+			// update gradient
+			double eta = 1.0/(L+m_lambda);
+			stepsCumSum += kappa * eta;//we delay update of the matrix
+			if(m_offset) noalias(model.offset()) -= eta * gradOffset;
+			kappa *= 1 - eta * m_lambda;//2-norm regularization
+			
+			//line-search procedure, 4.6 in the paper
+			noalias(f_b) -= derivative/L*pointNorms(b);
+			double newValue = loss.eval(point.label, f_b);
+			if(norm_sqr(derivative)*pointNorms(b) > 1.e-8 && newValue > currentValue - 1/(2*L)*norm_sqr(derivative)*pointNorms(b)){
 				L *= 2;
 			}
 			L*= std::pow(2.0,-1.0/ell);//allow L to slightly shrink in case our initial estimate was too large
 			
+			
+			//every epoch we reset the internal variables for numeric stability and apply all outstanding updates
+			//this also ensures that in the last iteration all updates are applied (as iterations is a multiple of ell)
+			if((iter +1)% ell == 0){
+				noalias(model.matrix()) -= (stepsCumSum - blas::repeat(appliedRates,labelDim))*grad;
+				model.matrix() *= kappa;
+				kappa = 1;
+				stepsCumSum = 0.0;
+				appliedRates.clear();
+			}
 		}
 	}
 	
