866078a6091118a435664e9571a3d30f1cdd3c37
Niels Bjørn Bugge Grathwohl <bjorn.bugge@alexandra.dk>
2017-02-27 09:58:29 +0100

    Update unittest of MOEAD

--- Test/Algorithms/DirectSearch/MOEAD.cpp
+++ Test/Algorithms/DirectSearch/MOEAD.cpp
@@ -11,79 +11,62 @@
 
 using namespace shark;
 
+typedef boost::mpl::list<DTLZ1, DTLZ2, ZDT1, ZDT2, ZDT3, ZDT4, ZDT6> obj_funs;
 
-BOOST_AUTO_TEST_CASE(HYPERVOLUME_Functions)
+double optimal_hyper_volume(std::size_t n, const DTLZ1 &) {
+    if(n == 10) return 120.86111;
+    if(n == 100) return 120.873737; 
+    return -1;
+}
+double optimal_hyper_volume(std::size_t n, const DTLZ2 &) { 
+    if(n == 10) return 120.178966;
+    if(n == 100) return 120.210644; 
+    return -1;
+}
+double optimal_hyper_volume(std::size_t n, const ZDT1 &) { 
+    if(n == 10) return 120.613761;
+    if(n == 100) return 120.662137; 
+    return -1;
+}
+double optimal_hyper_volume(std::size_t n, const ZDT2 &) { 
+    if(n == 10) return 120.286820;
+    if(n == 100) return 120.328881; 
+    return -1;
+}
+double optimal_hyper_volume(std::size_t n, const ZDT3 &) { 
+    if(n == 10) return 128.748470;
+    if(n == 100) return 128.775955; 
+    return -1;
+}
+double optimal_hyper_volume(std::size_t n, const ZDT4 &) { 
+    if(n == 10) return 120.613761;
+    if(n == 100) return 120.662137; 
+    return -1;
+}
+double optimal_hyper_volume(std::size_t n, const ZDT6 &) { 
+    if(n == 10) return 117.483246;
+    if(n == 100) return 117.514950; 
+    return -1;
+}
+
+
+BOOST_AUTO_TEST_SUITE (Algorithms_DirectSearch_MOEAD)
+
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(Hypervolume_functions_mu100, OF, obj_funs)
 {
-    const std::size_t reps = 10;
+    const std::size_t reps = 5;
     const std::size_t mu = 100;
     const std::size_t T = 20;
     const std::size_t iters = 450 * mu;
     const RealVector reference{11, 11};
-    {
-        DTLZ1 function(5);
-//        const double volume = 120.861111; // mu = 10
-        const double volume = 120.873737; // mu = 100
-        MOEAD optimizer;
-        optimizer.mu() = mu;
-        optimizer.neighbourhoodSize() = T;
-        testFunction(optimizer, function, reference, volume, reps, iters, 5.e-3);
-    }
-    {
-        DTLZ2 function(5);
-//        const double volume = 120.178966; // mu = 10
-        const double volume = 120.210644; // mu = 100
-        MOEAD optimizer;
-        optimizer.mu() = mu;
-        optimizer.neighbourhoodSize() = T;
-        testFunction(optimizer, function, reference, volume, reps, iters, 5.e-3);
-    }
-    {
-        ZDT1 function(5);
-//		const double volume = 120.613761; // mu = 10
-        const double volume = 120.662137; // mu = 100
-        MOEAD optimizer;
-        optimizer.mu() = mu;
-        optimizer.neighbourhoodSize() = T;
-        testFunction(optimizer, function, reference, volume, reps, iters, 5.e-3);
-    }
-	{
-		ZDT2 function(5);
-//		const double volume = 120.286820; // mu = 10
-        const double volume = 120.328881; // mu = 100
-        MOEAD optimizer;
-		optimizer.mu() = mu;
-        optimizer.neighbourhoodSize() = T;
-		testFunction(optimizer, function, reference, volume, reps, iters, 5.e-3);
-	}
-	{
-		ZDT3 function(5);
-//		const double volume = 128.748470; // mu = 10
-        const double volume = 128.775955; // mu = 100
-		MOEAD optimizer;
-		optimizer.mu() = mu;
-        optimizer.neighbourhoodSize() = T;
-		testFunction(optimizer, function, reference, volume, reps, iters, 5.e-3);
-	}
-    {
-        ZDT4 function(5);
-//        const double volume = 120.613761; // mu = 10
-        const double volume = 120.662137; // mu = 100
-        MOEAD optimizer;
-        optimizer.mu() = mu;
-        optimizer.neighbourhoodSize() = T;
-        testFunction(optimizer, function, reference, volume, reps, iters, 5.e-3);
-    }
-    {
-        ZDT6 function(5);
-//        const double volume = 117.483246; // mu = 10
-        const double volume = 117.514950; // mu = 100
-        MOEAD optimizer;
-        optimizer.mu() = mu;
-        optimizer.neighbourhoodSize() = T;
-        testFunction(optimizer, function, reference, volume, reps, iters, 5.e-3);
-    }
+    OF function(5);
+    const double volume = optimal_hyper_volume(mu, function);
+    MOEAD optimizer;
+    optimizer.mu() = mu;
+    optimizer.neighbourhoodSize() = T;
+    testFunction(optimizer, function, reference, volume, reps, iters, 5.e-3);
 }
-
 
 
 
