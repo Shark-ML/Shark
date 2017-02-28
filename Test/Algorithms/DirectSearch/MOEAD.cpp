2ec42877b5a6207ccae3deb049517d3dba57d93b
Niels Bjørn Bugge Grathwohl <bjorn.bugge@alexandra.dk>
2017-02-28 10:04:26 +0100

    Spaces  ->  tabs !

--- Test/Algorithms/DirectSearch/MOEAD.cpp
+++ Test/Algorithms/DirectSearch/MOEAD.cpp
@@ -14,39 +14,39 @@ using namespace shark;
 typedef boost::mpl::list<DTLZ1, DTLZ2, ZDT1, ZDT2, ZDT3, ZDT4, ZDT6> obj_funs;
 
 double optimal_hyper_volume(std::size_t n, const DTLZ1 &) {
-    if(n == 10) return 120.86111;
-    if(n == 100) return 120.873737; 
-    return -1;
+	if(n == 10) return 120.86111;
+	if(n == 100) return 120.873737;
+	return -1;
 }
-double optimal_hyper_volume(std::size_t n, const DTLZ2 &) { 
-    if(n == 10) return 120.178966;
-    if(n == 100) return 120.210644; 
-    return -1;
+double optimal_hyper_volume(std::size_t n, const DTLZ2 &) {
+	if(n == 10) return 120.178966;
+	if(n == 100) return 120.210644;
+	return -1;
 }
-double optimal_hyper_volume(std::size_t n, const ZDT1 &) { 
-    if(n == 10) return 120.613761;
-    if(n == 100) return 120.662137; 
-    return -1;
+double optimal_hyper_volume(std::size_t n, const ZDT1 &) {
+	if(n == 10) return 120.613761;
+	if(n == 100) return 120.662137;
+	return -1;
 }
-double optimal_hyper_volume(std::size_t n, const ZDT2 &) { 
-    if(n == 10) return 120.286820;
-    if(n == 100) return 120.328881; 
-    return -1;
+double optimal_hyper_volume(std::size_t n, const ZDT2 &) {
+	if(n == 10) return 120.286820;
+	if(n == 100) return 120.328881;
+	return -1;
 }
-double optimal_hyper_volume(std::size_t n, const ZDT3 &) { 
-    if(n == 10) return 128.748470;
-    if(n == 100) return 128.775955; 
-    return -1;
+double optimal_hyper_volume(std::size_t n, const ZDT3 &) {
+	if(n == 10) return 128.748470;
+	if(n == 100) return 128.775955;
+	return -1;
 }
-double optimal_hyper_volume(std::size_t n, const ZDT4 &) { 
-    if(n == 10) return 120.613761;
-    if(n == 100) return 120.662137; 
-    return -1;
+double optimal_hyper_volume(std::size_t n, const ZDT4 &) {
+	if(n == 10) return 120.613761;
+	if(n == 100) return 120.662137;
+	return -1;
 }
-double optimal_hyper_volume(std::size_t n, const ZDT6 &) { 
-    if(n == 10) return 117.483246;
-    if(n == 100) return 117.514950; 
-    return -1;
+double optimal_hyper_volume(std::size_t n, const ZDT6 &) {
+	if(n == 10) return 117.483246;
+	if(n == 100) return 117.514950;
+	return -1;
 }
 
 
@@ -55,17 +55,17 @@ BOOST_AUTO_TEST_SUITE (Algorithms_DirectSearch_MOEAD)
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(Hypervolume_functions_mu10, OF, obj_funs)
 {
-    const std::size_t reps = 5;
-    const std::size_t mu = 10;
-    const std::size_t T = 5;
-    const std::size_t iters = 1000 * mu;
-    const RealVector reference{11, 11};
-    OF function(5);
-    const double volume = optimal_hyper_volume(mu, function);
-    MOEAD optimizer;
-    optimizer.mu() = mu;
-    optimizer.neighbourhoodSize() = T;
-    testFunction(optimizer, function, reference, volume, reps, iters, 5.e-2);
+	const std::size_t reps = 5;
+	const std::size_t mu = 10;
+	const std::size_t T = 5;
+	const std::size_t iters = 1000 * mu;
+	const RealVector reference{11, 11};
+	OF function(5);
+	const double volume = optimal_hyper_volume(mu, function);
+	MOEAD optimizer;
+	optimizer.mu() = mu;
+	optimizer.neighbourhoodSize() = T;
+	testFunction(optimizer, function, reference, volume, reps, iters, 5.e-2);
 }
 
 
--- Test/Algorithms/DirectSearch/Operators/Grid.cpp
+++ Test/Algorithms/DirectSearch/Operators/Grid.cpp
@@ -6,6 +6,7 @@
 #include <shark/Algorithms/DirectSearch/Operators/Grid.h>
 #include <shark/ObjectiveFunctions/Benchmarks/Benchmarks.h>
 #include <shark/Rng/Rng.h>
+#include <shark/LinAlg/Metrics.h>
 
 #include <iostream>
 
@@ -13,180 +14,202 @@ using namespace shark;
 
 BOOST_AUTO_TEST_SUITE (Algorithms_DirectSearch_Operators_Grid)
 
+BOOST_AUTO_TEST_CASE(unitVectors_correct)
+{
+	for(std::size_t n = 2; n < 6; ++n)
+	{
+		for(std::size_t s = 3; s < 10; ++s)
+		{
+			RealMatrix weights = weightLattice(n, s);
+			RealMatrix refvecs = unitVectors(n, s);
+			BOOST_CHECK_EQUAL(weights.size1(), refvecs.size1());
+			BOOST_CHECK_EQUAL(weights.size2(), refvecs.size2());
+			const RealVector w(refvecs.size2(), 1);
+			// All vectors must be of length 1 since they are unit vectors...
+			for(std::size_t r = 0; r < refvecs.size1(); ++r)
+			{
+				double l = remora::diagonalMahalanobisNorm(
+					remora::row(refvecs, r), w);
+				BOOST_CHECK_CLOSE(l, 1, 1e-11);
+			}
+		}
+	}
+}
+
 BOOST_AUTO_TEST_CASE(pointLattice_correct)
 {
-    for(std::size_t n = 1; n < 6; ++n)
-    {
-        for(std::size_t sum = 3; sum < 10; ++sum)
-        {
-            UIntMatrix m = pointLattice(n, sum);
-            for(std::size_t row = 0; row < m.size1(); ++row)
-            {
-                std::size_t actual_sum = 0;
-                for(std::size_t col = 0; col < m.size2(); ++col)
-                {
-                    actual_sum += m(row, col);
-                }
-                BOOST_CHECK_EQUAL(actual_sum, sum);
-            }   
-        }
-    }
+	for(std::size_t n = 1; n < 6; ++n)
+	{
+		for(std::size_t sum = 3; sum < 10; ++sum)
+		{
+			UIntMatrix m = pointLattice(n, sum);
+			for(std::size_t row = 0; row < m.size1(); ++row)
+			{
+				std::size_t actual_sum = 0;
+				for(std::size_t col = 0; col < m.size2(); ++col)
+				{
+					actual_sum += m(row, col);
+				}
+				BOOST_CHECK_EQUAL(actual_sum, sum);
+			}
+		}
+	}
 }
 
 BOOST_AUTO_TEST_CASE(sampleUniformly_correct)
 {
-    DefaultRngType rng;
-    for(std::size_t n = 2; n < 5; ++n)
-    {
-        for(std::size_t sum = n; sum < 15; ++sum)
-        {
-            UIntMatrix m = pointLattice(n, sum);
-            UIntMatrix sampled = sampleUniformly(rng, m, sum);
-            BOOST_CHECK_EQUAL(sampled.size1(), sum);
-            std::size_t num_corners_orig = 0;
-            std::size_t num_corners_sampled = 0;
-            for(std::size_t r = 0; r < sampled.size1(); ++r)
-            {
-                if(detail::isCorner(sampled.row_begin(r), sampled.row_end(r)))
-                {
-                    ++num_corners_sampled;
-                }
-            }
-            for(std::size_t r = 0; r < m.size1(); ++r)
-            {
-                if(detail::isCorner(m.row_begin(r), m.row_end(r)))
-                {
-                    ++num_corners_orig;
-                }
-            }
-            BOOST_CHECK_EQUAL(num_corners_orig, n);
-            BOOST_CHECK_EQUAL(num_corners_sampled, n);
-        }
-    }
+	DefaultRngType rng;
+	for(std::size_t n = 2; n < 5; ++n)
+	{
+		for(std::size_t sum = n; sum < 15; ++sum)
+		{
+			UIntMatrix m = pointLattice(n, sum);
+			UIntMatrix sampled = sampleUniformly(rng, m, sum);
+			BOOST_CHECK_EQUAL(sampled.size1(), sum);
+			std::size_t num_corners_orig = 0;
+			std::size_t num_corners_sampled = 0;
+			for(std::size_t r = 0; r < sampled.size1(); ++r)
+			{
+				if(detail::isCorner(sampled.row_begin(r), sampled.row_end(r)))
+				{
+					++num_corners_sampled;
+				}
+			}
+			for(std::size_t r = 0; r < m.size1(); ++r)
+			{
+				if(detail::isCorner(m.row_begin(r), m.row_end(r)))
+				{
+					++num_corners_orig;
+				}
+			}
+			BOOST_CHECK_EQUAL(num_corners_orig, n);
+			BOOST_CHECK_EQUAL(num_corners_sampled, n);
+		}
+	}
 }
 
 
 BOOST_AUTO_TEST_CASE(pointLattice_has_expected_size)
 {
-    for(std::size_t mu_prime = 3; mu_prime < 10; ++mu_prime)
-    {
-        for(std::size_t d = 2; d < 5; ++d)
-        {
-            UIntMatrix l = pointLattice(d, mu_prime);
-            std::size_t expected_size = detail::sumlength(d, mu_prime);
-            BOOST_CHECK_EQUAL(expected_size, l.size1());
-        }
-    }
+	for(std::size_t mu_prime = 3; mu_prime < 10; ++mu_prime)
+	{
+		for(std::size_t d = 2; d < 5; ++d)
+		{
+			UIntMatrix l = pointLattice(d, mu_prime);
+			std::size_t expected_size = detail::sumlength(d, mu_prime);
+			BOOST_CHECK_EQUAL(expected_size, l.size1());
+		}
+	}
 }
 
 BOOST_AUTO_TEST_CASE(weights_expected_size)
 {
-    for(std::size_t n = 2; n < 6; ++n)
-    {
-        for(std::size_t sum = 3; sum < 15; ++sum)
-        {
-            std::size_t exp = detail::sumlength(n, sum);
-            RealMatrix w = weightLattice(n, sum);
-            BOOST_CHECK_EQUAL(w.size1(), exp);
-        }
-    }
+	for(std::size_t n = 2; n < 6; ++n)
+	{
+		for(std::size_t sum = 3; sum < 15; ++sum)
+		{
+			std::size_t exp = detail::sumlength(n, sum);
+			RealMatrix w = weightLattice(n, sum);
+			BOOST_CHECK_EQUAL(w.size1(), exp);
+		}
+	}
 }
 
 BOOST_AUTO_TEST_CASE(best_point_count_2d_correct)
 {
-    for(std::size_t i = 1; i < 100; ++i)
-    {
-        const std::size_t pc = bestPointSumForLattice(2, i);
-        BOOST_CHECK_EQUAL(i, detail::sumlength(2, pc));
-    }
+	for(std::size_t i = 1; i < 100; ++i)
+	{
+		const std::size_t pc = bestPointSumForLattice(2, i);
+		BOOST_CHECK_EQUAL(i, detail::sumlength(2, pc));
+	}
 }
 
 BOOST_AUTO_TEST_CASE(best_point_count)
 {
-    for(std::size_t n = 3; n < 5; ++n)
-    {
-        for(std::size_t sum = 3; sum < 10; ++sum)
-        {
-            std::size_t b = bestPointSumForLattice(n, sum);
-            RealMatrix w = weightLattice(n, b);
-            BOOST_CHECK(detail::sumlength(n, b) > sum);
-            BOOST_CHECK_EQUAL(w.size1(), detail::sumlength(n, b));
-        }
-    }
+	for(std::size_t n = 3; n < 5; ++n)
+	{
+		for(std::size_t sum = 3; sum < 10; ++sum)
+		{
+			std::size_t b = bestPointSumForLattice(n, sum);
+			RealMatrix w = weightLattice(n, b);
+			BOOST_CHECK(detail::sumlength(n, b) > sum);
+			BOOST_CHECK_EQUAL(w.size1(), detail::sumlength(n, b));
+		}
+	}
 }
 
 
 BOOST_AUTO_TEST_CASE(lattice_sum_to_one)
 {
-    const std::size_t mu_prime = 12;
-    const std::size_t n_max = 10;
-    for(std::size_t n = 2; n < n_max; ++n)
-    {
-        const RealMatrix weights = weightLattice(n, mu_prime);
-        for(std::size_t row = 0; row < weights.size1(); ++row)
-        {
-            double sum = 0;
-            std::for_each(weights.row_begin(row), weights.row_end(row),
-                          [&sum](double d)
-                          {
-                              sum += d;
-                          });
-            BOOST_CHECK_CLOSE(1.0, sum, 1e-8);
-        }
-    }
+	const std::size_t mu_prime = 12;
+	const std::size_t n_max = 10;
+	for(std::size_t n = 2; n < n_max; ++n)
+	{
+		const RealMatrix weights = weightLattice(n, mu_prime);
+		for(std::size_t row = 0; row < weights.size1(); ++row)
+		{
+			double sum = 0;
+			std::for_each(weights.row_begin(row), weights.row_end(row),
+						  [&sum](double d)
+						  {
+							  sum += d;
+						  });
+			BOOST_CHECK_CLOSE(1.0, sum, 1e-8);
+		}
+	}
 }
 
 BOOST_AUTO_TEST_CASE(vector_sorting_correct)
 {
-    const std::size_t mu_prime = 8;
-    const std::size_t n_max = 4;
-    for(std::size_t n = 2; n < n_max; ++n)
-    {
-        const RealMatrix weights = weightLattice(mu_prime, n);
-        for(std::size_t T = 1; T <= weights.size1() / 2 && T <= 30; ++T)
-        {
-            UIntMatrix dists = computeClosestNeighbourIndices(weights, T);
-            for(std::size_t row = 0; row < dists.size1(); ++row)
-            {
-                std::list<std::vector<double>> my_nearest_points;
-                std::for_each(dists.row_begin(row), dists.row_end(row),
-                              [&](std::size_t idx)
-                              {
-                                  my_nearest_points.push_back(
-                                      std::vector<double>(weights.row_begin(idx),
-                                                          weights.row_end(idx)));
-                              });
-                BOOST_CHECK_EQUAL(my_nearest_points.size(), T);
-                const std::vector<double> this_point(weights.row_begin(row),
-                                                     weights.row_end(row));
-                std::list<double> my_dists;
-                for(std::vector<double> const & point : my_nearest_points)
-                {
-                    double d = 0;
-                    for(std::size_t i = 0; i < point.size(); ++i)
-                    {
-                        d += std::pow(this_point[i] - point[i], 2);
-                    }
-                    my_dists.push_back(std::sqrt(d));
-                }
-                BOOST_CHECK_EQUAL(my_dists.size(), T);
-                std::vector<double> my_dists_vec(my_dists.begin(), my_dists.end());
-                for(std::size_t i = 1; i < my_dists_vec.size(); ++i)
-                {
-                    double delta = my_dists_vec[i] - my_dists_vec[i - 1];
-                    // delta is negative if the vector is *not* sorted
-                    if(delta < 0)
-                    {
-                        // Check whether it's just due to floating point stuff.
-                        // (This is why we don't just use std::is_sorted)
-                        BOOST_CHECK_CLOSE_FRACTION(my_dists_vec[i], 
-                                                   my_dists_vec[i - 1], 1e-8);
-                    }
-                }
-            }
-        }
-        
-    }
+	const std::size_t mu_prime = 8;
+	const std::size_t n_max = 4;
+	for(std::size_t n = 2; n < n_max; ++n)
+	{
+		const RealMatrix weights = weightLattice(mu_prime, n);
+		for(std::size_t T = 1; T <= weights.size1() / 2 && T <= 30; ++T)
+		{
+			UIntMatrix dists = computeClosestNeighbourIndices(weights, T);
+			for(std::size_t row = 0; row < dists.size1(); ++row)
+			{
+				std::list<std::vector<double>> my_nearest_points;
+				std::for_each(dists.row_begin(row), dists.row_end(row),
+							  [&](std::size_t idx)
+							  {
+								  my_nearest_points.push_back(
+									  std::vector<double>(weights.row_begin(idx),
+														  weights.row_end(idx)));
+							  });
+				BOOST_CHECK_EQUAL(my_nearest_points.size(), T);
+				const std::vector<double> this_point(weights.row_begin(row),
+													 weights.row_end(row));
+				std::list<double> my_dists;
+				for(std::vector<double> const & point : my_nearest_points)
+				{
+					double d = 0;
+					for(std::size_t i = 0; i < point.size(); ++i)
+					{
+						d += std::pow(this_point[i] - point[i], 2);
+					}
+					my_dists.push_back(std::sqrt(d));
+				}
+				BOOST_CHECK_EQUAL(my_dists.size(), T);
+				std::vector<double> my_dists_vec(my_dists.begin(), my_dists.end());
+				for(std::size_t i = 1; i < my_dists_vec.size(); ++i)
+				{
+					double delta = my_dists_vec[i] - my_dists_vec[i - 1];
+					// delta is negative if the vector is *not* sorted
+					if(delta < 0)
+					{
+						// Check whether it's just due to floating point stuff.
+						// (This is why we don't just use std::is_sorted)
+						BOOST_CHECK_CLOSE_FRACTION(my_dists_vec[i],
+												   my_dists_vec[i - 1], 1e-8);
+					}
+				}
+			}
+		}
+
+	}
 }
 
 
