ddc9b250cdcb440f8c9986d5cdc4c3dafed2d2ae
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2018-01-16 16:59:01 +0100

    Implement findLeaf for CARTree

--- Test/Models/RFClassifier.cpp
+++ Test/Models/RFClassifier.cpp
@@ -91,6 +91,15 @@ BOOST_AUTO_TEST_CASE( RF_Classifier ) {
 	BOOST_REQUIRE_CLOSE(error_train_serialized, error_train, 1.e-13);
 	BOOST_REQUIRE_CLOSE(error_train2_serialized, error_train2, 1.e-13);
 	BOOST_REQUIRE_CLOSE(error_test_serialized, error_test, 1.e-13);
+
+	// Find the leaf for a sample
+	for(auto m=0; m<model.numberOfModels(); ++m){
+	auto aTree = model.getModel(m);
+	auto result = aTree.findLeaf(test.inputs().element(0));
+	BOOST_CHECK(result < aTree.numberOfNodes());
+	std::cout << "Result : " << result << " < " << aTree.numberOfNodes() << '\n';	
+	}
+        
 }
 
 BOOST_AUTO_TEST_SUITE_END()
